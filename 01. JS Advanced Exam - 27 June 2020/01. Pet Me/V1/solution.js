d3b73677d4acae11e37e5dc2eba739d13fdb80bf
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-07 12:41:29 +0300

    Add solution to task 1: Pet Me

--- 01. JS Advanced Exam - 27 June 2020/01. Pet Me/V1/solution.js
+++ 01. JS Advanced Exam - 27 June 2020/01. Pet Me/V1/solution.js
@@ -1,4 +1,50 @@
 function solve() {
-    // TODO ...
-}
+    const container = document.getElementById('container');
+    const [name, age, kind, owner] = container.querySelectorAll('input');
+    const addBtn = container.querySelector('button');
+    const adoptionList = document.querySelector('#adoption > ul');
+    const adoptedList = document.querySelector('#adopted > ul');
 
+    addBtn.addEventListener('click', e => {
+        e.preventDefault();
+
+        if (name.value !== '' && age.value !== '' && !isNaN(+age.value) && kind.value !== '' && owner.value !== '') {
+            adoptionList.innerHTML += `<li>
+                <p><strong>${name.value}</strong> is a <strong>${age.value}</strong> year old <strong>${kind.value}</strong></p>
+                <span>Owner: ${owner.value}</span>
+                <button>Contact with owner</button>
+           </li>`;
+
+            name.value = '';
+            age.value = '';
+            kind.value = '';
+            owner.value = '';
+        }
+    });
+
+    adoptionList.addEventListener('click', (e) => {
+        if (e.target.tagName === 'BUTTON' && e.target.textContent === 'Contact with owner') {
+            const li = e.target.parentNode;
+
+            li.querySelector('button').remove();
+            li.innerHTML += `<div>
+                <input placeholder="Enter your names">
+                <button>Yes! I take it!</button>
+            </div>`;
+        } else if (e.target.tagName === 'BUTTON' && e.target.textContent === 'Yes! I take it!') {
+            const li = e.target.parentNode.parentNode;
+            const input = li.querySelector('input');
+
+            if (input.value !== '') {
+                li.querySelector('div').remove();
+                li.querySelector('span').textContent = `New Owner: ${input.value}`;
+                li.innerHTML += `<button>Checked</button>`;
+                adoptedList.appendChild(li);
+            }
+        }
+    });
+
+    adoptedList.addEventListener('click', (e) => {
+        if (e.target.tagName === 'BUTTON' && e.target.textContent === 'Checked') e.target.parentNode.remove();
+    });
+}
