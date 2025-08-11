95881194ce1a77c941c8bdd62ddbb070fb49f74c
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-11 15:42:02 +0300

    Task 1 add a new task (article) in “Open” section.

--- 03. JS Advanced Retake - 08 April 2020/01. Task Manager/Skeleton/app.js
+++ 03. JS Advanced Retake - 08 April 2020/01. Task Manager/Skeleton/app.js
@@ -1,3 +1,24 @@
 function solve() {
-    console.log("//TODO")
+    const task = document.getElementById("task");
+    const description = document.getElementById("description");
+    const date = document.getElementById("date");
+    const add = document.getElementById("add");
+
+    const [_, orange, yellow, green] = document.querySelectorAll("section");
+
+    add.addEventListener("click", e => {
+        e.preventDefault();
+
+        if (task.value !== '' && description.value !== '' && date.value !== '') {
+            orange.querySelectorAll("div")[1].innerHTML += `<article>
+<h3>${task.value}</h3>
+<p>Description: ${description.value}</p>
+<p>Due Date: ${date.value}</p>
+<div class="flex">
+<button class="green">Start</button>
+<button class="red">Delete</button>
+</div>
+</article>`;
+        }
+    });
 }
