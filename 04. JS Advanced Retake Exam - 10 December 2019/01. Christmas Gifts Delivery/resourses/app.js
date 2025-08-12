4a8e8472de116946c4e4d2384130018803a05abf
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-12 20:50:22 +0300

    Task 1 add a new list item to the List of gifts

--- 04. JS Advanced Retake Exam - 10 December 2019/01. Christmas Gifts Delivery/resourses/app.js
+++ 04. JS Advanced Retake Exam - 10 December 2019/01. Christmas Gifts Delivery/resourses/app.js
@@ -1,3 +1,17 @@
 function solution() {
-    //TO DO
+    const [addGifts, listOfGifts, sentGifts, discardedGifts] = document.querySelectorAll('section');
+    const input = addGifts.querySelector('input');
+
+    addGifts.querySelector('button').addEventListener('click', e => {
+        if (input.value.trim() !== '') {
+            const ul = listOfGifts.querySelector('ul');
+
+            ul.innerHTML += `<li class="gift">${input.value.trim()}
+<button id="sendButton">Send</button>
+<button id="discardButton">Discard</button>
+</li>`;
+            Array.from(ul.getElementsByTagName("LI")).sort((a, b) => a.firstChild.textContent.trim().localeCompare(b.firstChild.textContent.trim())).forEach(li => ul.appendChild(li));
+            input.value = '';
+        }
+    });
 }
