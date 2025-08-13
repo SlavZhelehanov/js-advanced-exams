02abd6b1d76129e1312f9a29a9fe5de3d9eeac94
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-13 15:45:17 +0300

    Task 1.1: Getting the information from the form

--- 05. JS Advanced Regular Exam - 20 October 2024/01.Active Minutes/resources/app.js
+++ 05. JS Advanced Regular Exam - 20 October 2024/01.Active Minutes/resources/app.js
@@ -1,5 +1,37 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  //TODO...
+    const activityType = document.getElementById("type");
+    const intensity = document.getElementById("intensity");
+    const calories = document.getElementById("calories");
+    const duration = document.getElementById("duration");
+    const date = document.getElementById("date");
+    const addActivity = document.getElementById("add-activity");
+    const previewActivity = document.getElementById("preview-activity");
+
+    addActivity.addEventListener("click", e => {
+        e.preventDefault();
+
+        if (activityType.value !== '' && intensity.value !== '' && calories.value !== '' && duration.value !== '' && date.value !== '') {
+            previewActivity.innerHTML = `<li>
+<article>
+<p>Activity: ${activityType.value}</p>
+<p>Intensity: ${intensity.value}</p>
+<p>Duration: ${duration.value} min.</p>
+<p>Date: ${date.value}</p>
+<p>Calories: ${calories.value}</p>
+</article>
+<div class="btn-container">
+<button class="edit-btn">Edit</button>
+<button class="next-btn">Next</button>
+</div>
+</li>`;
+            activityType.value = '';
+            intensity.value = '';
+            calories.value = '';
+            duration.value = '';
+            date.value = '';
+            addActivity.disabled = true;
+        }
+    });
 }
