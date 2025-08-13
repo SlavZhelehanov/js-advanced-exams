fbec00d21ffbfcedd0afc2fee0f14c5f8d48c49b
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-13 17:08:26 +0300

    Task 2.3: showAvailableRooms()

--- 05. JS Advanced Regular Exam - 20 October 2024/02.Hotel Operations/hotelOperations.js
+++ 05. JS Advanced Regular Exam - 20 October 2024/02.Hotel Operations/hotelOperations.js
@@ -36,12 +36,25 @@ class Hotel {
 
         return `Great idea! Now with the ${roomType}, we have ${Object.keys(this.roomAvailability).length} types of rooms available, any other ideas?`;
     }
+
+    showAvailableRooms() {
+        if (0 === Object.keys(this.roomAvailability).length) return "Our rooms are not ready yet, please come back later...";
+
+        let output = [];
+
+        for (const [roomType, {neededSupplies, pricePerNight}] of Object.entries(this.roomAvailability)) {
+            output.push(`${roomType} - $ ${pricePerNight}`);
+        }
+        return output.join("\n");
+    }
 }
 
+//Input 1
 // let hotel = new Hotel(500);
 //
 // console.log(hotel.restockSupplies(["Soap 100 50", "Towels 20 100", "Shampoo 50 75"]));
 
+//Input 2
 // let hotel = new Hotel(500);
 //
 // console.log(hotel.restockSupplies(["Soap 100 50", "Towels 20 100", "Shampoo 50 75"]));
@@ -49,3 +62,13 @@ class Hotel {
 // console.log(hotel.addRoomType("Deluxe Suite", ["Soap 5", "Towels 2"], 200));
 // console.log(hotel.addRoomType("Standard Room", ["Soap 2", "Towels 1"], 100));
 // console.log(hotel.addRoomType("Standard Room", ["Soap 2", "Towels 1"], 100));
+
+//Input 3
+// let hotel = new Hotel(500);
+//
+// console.log(hotel.restockSupplies(["Soap 100 50", "Towels 20 100", "Shampoo 50 75"]));
+//
+// console.log(hotel.addRoomType("Deluxe Suite", ["Soap 5", "Towels 2"], 200));
+// console.log(hotel.addRoomType("Standard Room", ["Soap 2", "Towels 1"], 100));
+// console.log(hotel.showAvailableRooms());
+
