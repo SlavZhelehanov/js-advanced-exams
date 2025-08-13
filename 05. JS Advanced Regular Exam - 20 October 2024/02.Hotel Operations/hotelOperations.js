aaa10be3df1f34633f2611d474a4b5ac2d676dcc
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-13 16:58:24 +0300

    Task 2.2: addRoomType(roomType, neededSupplies, pricePerNight)

--- 05. JS Advanced Regular Exam - 20 October 2024/02.Hotel Operations/hotelOperations.js
+++ 05. JS Advanced Regular Exam - 20 October 2024/02.Hotel Operations/hotelOperations.js
@@ -23,4 +23,29 @@ class Hotel {
         });
         return result.join('\n');
     }
+
+    addRoomType(roomType, neededSupplies, pricePerNight) {
+        if (this.roomAvailability.hasOwnProperty(roomType)) {
+            return `The ${roomType} is already available in our hotel, try something different.`;
+        }
+
+        this.roomAvailability[roomType] = {
+            neededSupplies: neededSupplies,
+            pricePerNight: pricePerNight
+        };
+
+        return `Great idea! Now with the ${roomType}, we have ${Object.keys(this.roomAvailability).length} types of rooms available, any other ideas?`;
+    }
 }
+
+// let hotel = new Hotel(500);
+//
+// console.log(hotel.restockSupplies(["Soap 100 50", "Towels 20 100", "Shampoo 50 75"]));
+
+// let hotel = new Hotel(500);
+//
+// console.log(hotel.restockSupplies(["Soap 100 50", "Towels 20 100", "Shampoo 50 75"]));
+//
+// console.log(hotel.addRoomType("Deluxe Suite", ["Soap 5", "Towels 2"], 200));
+// console.log(hotel.addRoomType("Standard Room", ["Soap 2", "Towels 1"], 100));
+// console.log(hotel.addRoomType("Standard Room", ["Soap 2", "Towels 1"], 100));
