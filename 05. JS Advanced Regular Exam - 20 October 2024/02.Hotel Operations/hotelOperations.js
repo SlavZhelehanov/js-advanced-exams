0673ad34bb8b4902a71d415a28da80285195cae0
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-13 16:42:13 +0300

    Task 2.1: restockSupplies(supplies)

--- 05. JS Advanced Regular Exam - 20 October 2024/02.Hotel Operations/hotelOperations.js
+++ 05. JS Advanced Regular Exam - 20 October 2024/02.Hotel Operations/hotelOperations.js
@@ -4,5 +4,23 @@ class Hotel {
         this.roomAvailability = {};
         this.supplyStock = {};
     }
-    
+
+    restockSupplies(supplies) {
+        let result = [];
+
+        supplies.forEach(item => {
+            const [supplyName, supplyQuantity, supplyTotalPrice] = item.split(' ');
+
+            if (0 <= this.initialBudget - +supplyTotalPrice && !this.supplyStock[supplyName]) {
+                this.supplyStock[supplyName] = +supplyQuantity;
+                this.initialBudget -= +supplyTotalPrice;
+                result.push(`Successfully stocked ${supplyQuantity} ${supplyName}`);
+            } else if (0 <= this.initialBudget - +supplyTotalPrice && this.supplyStock[supplyName]) {
+                this.supplyStock[supplyName] += +supplyQuantity;
+                this.initialBudget -= +supplyTotalPrice;
+                result.push(`Successfully stocked ${supplyQuantity} ${supplyName}`);
+            } else result.push(`There was not enough money to restock ${supplyQuantity} ${supplyName}`);
+        });
+        return result.join('\n');
+    }
 }
