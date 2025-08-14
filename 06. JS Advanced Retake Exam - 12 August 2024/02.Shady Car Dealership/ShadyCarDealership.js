38af0486f78fedae03bde60b264185133835935a
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-14 10:35:38 +0300

    Task 2.2: addCar (model, year, mileage, price

--- 06. JS Advanced Retake Exam - 12 August 2024/02.Shady Car Dealership/ShadyCarDealership.js
+++ 06. JS Advanced Retake Exam - 12 August 2024/02.Shady Car Dealership/ShadyCarDealership.js
@@ -5,4 +5,20 @@ class ShadyCarDealership {
         this.soldCars = [];
         this.revenue = 0;
     }
+
+    addCar(model, year, mileage, price) {
+        if (model === '' || year <= 1950 || mileage < 0 || price < 0) {
+            throw new Error('Invalid car!');
+        } else {
+            this.availableCars.push({ model, year, mileage, price });
+            return `New car added: ${model} (${year}) / ${mileage} km. - ${price.toFixed(2)}$`;
+        }
+    }
 }
+
+// Input 1
+// const dealership = new ShadyCarDealership('Shady Motors');
+// console.log(dealership.addCar('Honda CR-V', 2010, 120000, 15000));
+// console.log(dealership.addCar('VW Golf', 2011, 130000, 12000));
+// console.log(dealership.addCar('BMW X3', 2005, 220000, 9000));
+// console.log(dealership.addCar('Toyota Yaris', 2015, 80000, 18000));
