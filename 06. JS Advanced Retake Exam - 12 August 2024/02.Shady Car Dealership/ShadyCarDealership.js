fa57809a63cb161b3cb7ce90e96687c0917b5f6f
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-14 10:34:20 +0300

    Task 2.1: Constructor
    Task 2: Shady Car Dealership

---
+++ 06. JS Advanced Retake Exam - 12 August 2024/02.Shady Car Dealership/ShadyCarDealership.js
@@ -0,0 +1,8 @@
+class ShadyCarDealership {
+    constructor(dealerName) {
+        this.dealerName = dealerName;
+        this.availableCars = [];
+        this.soldCars = [];
+        this.revenue = 0;
+    }
+}
