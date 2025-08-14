b296fe611346fc7690b72d3ca671cd734b1101ae
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-14 10:38:40 +0300

    Add task 2.3: sellCar (model, desiredYear)

--- 06. JS Advanced Retake Exam - 12 August 2024/02.Shady Car Dealership/ShadyCarDealership.js
+++ 06. JS Advanced Retake Exam - 12 August 2024/02.Shady Car Dealership/ShadyCarDealership.js
@@ -14,6 +14,28 @@ class ShadyCarDealership {
             return `New car added: ${model} (${year}) / ${mileage} km. - ${price.toFixed(2)}$`;
         }
     }
+
+    sellCar(model, desiredYear) {
+        let foundCar = this.availableCars.find(car => car.model === model);
+
+        if (!foundCar) {
+            return(`${model} was not found!`);
+        } else {
+            let soldPrice = foundCar.price;
+            if (foundCar.year >= desiredYear) {
+                // Price stays the same
+            } else if (foundCar.year < desiredYear && desiredYear - foundCar.year <= 5) {
+                soldPrice *= 0.9; // 10% discount
+            } else if (foundCar.year < desiredYear && desiredYear - foundCar.year > 5) {
+                soldPrice *= 0.8; // 20% discount
+            }
+
+            this.soldCars.push({ model: foundCar.model, year: foundCar.year, mileage: foundCar.mileage, soldPrice });
+            this.availableCars = this.availableCars.filter(car => car.model !== model);
+            this.revenue += soldPrice;
+            return `${model} (${foundCar.year}) was sold for ${soldPrice.toFixed(2)}$`;
+        }
+    }
 }
 
 // Input 1
@@ -22,3 +44,12 @@ class ShadyCarDealership {
 // console.log(dealership.addCar('VW Golf', 2011, 130000, 12000));
 // console.log(dealership.addCar('BMW X3', 2005, 220000, 9000));
 // console.log(dealership.addCar('Toyota Yaris', 2015, 80000, 18000));
+
+// Input 2
+// const dealership = new ShadyCarDealership('Shady Motors');
+// console.log(dealership.addCar('Honda CR-V', 2010, 120000, 15000));
+// console.log(dealership.addCar('VW Golf', 2011, 130000, 12000));
+// console.log(dealership.addCar('BMW X3', 2005, 220000, 9000));
+// console.log(dealership.addCar('Toyota Yaris', 2015, 80000, 18000));
+// console.log(dealership.prepareCarForSale('Honda CR-V'));
+// console.log(dealership.prepareCarForSale('Honda Jazz'));
