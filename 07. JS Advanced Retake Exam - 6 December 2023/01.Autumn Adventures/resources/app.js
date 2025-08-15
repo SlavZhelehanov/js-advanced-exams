48237c15c39b0cf60b6599cc44121bd02424ff16
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-15 16:11:07 +0300

    Task 1.1: Getting the information from the form

--- 07. JS Advanced Retake Exam - 6 December 2023/01.Autumn Adventures/resources/app.js
+++ 07. JS Advanced Retake Exam - 6 December 2023/01.Autumn Adventures/resources/app.js
@@ -1,7 +1,31 @@
 window.addEventListener('load', solve);
 
 function solve() {
+    const [time, date, place, eventName, email, addBtn] = document.querySelectorAll('input');
+    const checkList = document.querySelector('#check-list');
 
+    addBtn.addEventListener('click', e => {
+        e.preventDefault();
+
+        if (time.value !== '' && date.value !== '' && place.value !== '' && eventName.value !== '' && email.value !== '') {
+            checkList.innerHTML = `<li class="event-content">
+<article>
+<p>Begins: ${date.value} at: ${time.value}</p>
+<p>In: ${place.value} </p>
+<p>Event: ${eventName.value} </p>
+<p>Contact: ${email.value} </p>
+</article>
+<button class="edit-btn">Edit</button>
+<button class="continue-btn">Continue</button>
+</li>`;
+            time.value = '';
+            date.value = '';
+            place.value = '';
+            eventName.value = '';
+            email.value = '';
+            addBtn.disabled = true;
+        }
+    });
 }
 
 
