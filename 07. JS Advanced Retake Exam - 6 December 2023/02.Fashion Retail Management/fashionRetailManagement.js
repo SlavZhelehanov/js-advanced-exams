8b1ee7c4ff3a19088be3ca7682b976ba74470de6
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-16 17:46:51 +0300

    Add task 2.1: Constructor
    Task 2: Fashion Retail Management

---
+++ 07. JS Advanced Retake Exam - 6 December 2023/02.Fashion Retail Management/fashionRetailManagement.js
@@ -0,0 +1,7 @@
+class FashionRetailInventory {
+    constructor(storehouse, location) {
+        this.storehouse = storehouse;
+        this.location = location;
+        this.productStock = [];
+    }
+}
