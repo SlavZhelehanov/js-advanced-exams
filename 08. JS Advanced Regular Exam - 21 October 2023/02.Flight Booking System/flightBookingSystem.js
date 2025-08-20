76d126dedd7096b7d98c97d4e54bce26376ccc38
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-20 13:43:39 +0300

    Add task 2.5: showBookings (criteria)
    And finalize the exam

--- 08. JS Advanced Regular Exam - 21 October 2023/02.Flight Booking System/flightBookingSystem.js
+++ 08. JS Advanced Regular Exam - 21 October 2023/02.Flight Booking System/flightBookingSystem.js
@@ -25,6 +25,37 @@ class FlightBookingSystem {
         this.bookingsCount--;
         return `Booking for passenger ${passengerName} on flight ${flightNumber} is cancelled.`;
     }
+
+    showBookings (criteria) {
+        if (0 === this.bookings.length) throw new Error(`No bookings have been made yet.`);
+        switch (criteria) {
+            case 'all': {
+                let message = `All bookings(${this.bookingsCount}):`;
+
+                for (const {passengerName, flightNumber} of this.bookings) message += `\n${passengerName} booked for flight ${flightNumber}.`;
+                return message;
+            }
+            case 'cheap': {
+                if (0 === this.flights.filter(f => f.price <= 100).length) return "No cheap bookings found.";
+                let message = "Cheap bookings:", cheap = this.flights.filter(f => f.price <= 100);
+
+                for (const {flightNumber, destination, departureTime, price} of cheap) {
+                    message += `\n${this.bookings.find(p => p.flightNumber === flightNumber).passengerName} booked for flight ${flightNumber}.`
+                }
+                return message;
+            }
+            case 'expensive': {
+                if (0 === this.flights.filter(f => 100 < f.price).length) return "No expensive bookings found.";
+                let message = "Expensive bookings:", expensive = this.flights.filter(f => 100 < f.price);
+
+                for (const {flightNumber, destination, departureTime, price} of expensive) {
+                    message += `\n${this.bookings.find(p => p.flightNumber === flightNumber).passengerName} booked for flight ${flightNumber}.`
+                }
+                return message;
+            }
+            default: {throw new Error("Invalid criteria.");}
+        }
+    }
 }
 
 // Input 1
@@ -49,3 +80,20 @@ class FlightBookingSystem {
 // console.log(system.bookFlight("Alice", "AA101"));
 // console.log(system.bookFlight("Bob", "BB202"));
 // console.log(system.cancelBooking("Alice", "AA101"));
+
+// Input 4
+// const system = new FlightBookingSystem("TravelWorld");
+// console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
+// console.log(system.addFlight("BB202", "New York", "10:30 AM", 180));
+// console.log(system.bookFlight("Alice", "AA101"));
+// console.log(system.bookFlight("Bob", "BB202"));
+// console.log(system.showBookings("all"));
+
+// Input 5
+// const system = new FlightBookingSystem("TravelWorld");
+// console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
+// console.log(system.addFlight("BB202", "New York", "10:30 AM", 180));
+// console.log(system.bookFlight("Alice", "AA101"));
+// console.log(system.bookFlight("Bob", "BB202"));
+// console.log(system.showBookings("expensive"));
+// console.log(system.showBookings("cheap"));
--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 - [x] 05. JS Advanced Regular Exam - 20 October 2024
 - [x] 06. JS Advanced Retake Exam - 12 August 2024
 - [ ] 07. JS Advanced Retake Exam - 6 December 2023
-- [ ] 08. JS Advanced Regular Exam - 21 October 2023
+- [x] 08. JS Advanced Regular Exam - 21 October 2023
