a58e65c3548ef579f13c01c1d987f4909baded8e
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-20 12:47:01 +0300

    Add task 2.2: addFlight(flightNumber, destination, departureTime, price)

--- 08. JS Advanced Regular Exam - 21 October 2023/02.Flight Booking System/flightBookingSystem.js
+++ 08. JS Advanced Regular Exam - 21 October 2023/02.Flight Booking System/flightBookingSystem.js
@@ -5,4 +5,17 @@ class FlightBookingSystem {
         this.bookings = [];
         this.bookingsCount = 0;
     }
+
+    addFlight(flightNumber, destination, departureTime, price) {
+        if (this.flights.some(flight => flight.flightNumber === flightNumber)) return `Flight ${flightNumber} to ${destination} is already available.`;
+        this.flights.push({flightNumber, destination, departureTime, price});
+        return `Flight ${flightNumber} to ${destination} has been added to the system.`;
+    }
 }
+
+// Input 1
+// const system = new FlightBookingSystem("TravelWorld");
+// console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
+// console.log(system.addFlight("BB202", "New York", "10:30 AM", 180));
+// console.log(system.addFlight("CC303", "Chicago", "11:45 AM", 120));
+// console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
