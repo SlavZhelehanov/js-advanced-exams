aec7950e4dd8a6675f2ac17bc27acf2883461817
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-20 13:01:03 +0300

    Add task 2.4: cancelBooking (passengerName, flightNumber)

--- 08. JS Advanced Regular Exam - 21 October 2023/02.Flight Booking System/flightBookingSystem.js
+++ 08. JS Advanced Regular Exam - 21 October 2023/02.Flight Booking System/flightBookingSystem.js
@@ -18,6 +18,13 @@ class FlightBookingSystem {
         this.bookingsCount++;
         return `Booking for passenger ${passengerName} on flight ${flightNumber} is confirmed.`;
     }
+
+    cancelBooking (passengerName, flightNumber) {
+        if (!this.bookings.some(passenger => passenger.flightNumber === flightNumber && passenger.passengerName === passengerName)) throw new Error(`Booking for passenger ${passengerName} on flight ${flightNumber} not found.`);
+        this.bookings = this.bookings.filter(booking => booking.flightNumber !== flightNumber && booking.passengerName !== passengerName);
+        this.bookingsCount--;
+        return `Booking for passenger ${passengerName} on flight ${flightNumber} is cancelled.`;
+    }
 }
 
 // Input 1
@@ -35,3 +42,10 @@ class FlightBookingSystem {
 // console.log(system.bookFlight("Bob", "BB202"));
 // console.log(system.bookFlight("Charlie", "CC303"));
 
+// Input 3
+// const system = new FlightBookingSystem("TravelWorld");
+// console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
+// console.log(system.addFlight("BB202", "New York", "10:30 AM", 180));
+// console.log(system.bookFlight("Alice", "AA101"));
+// console.log(system.bookFlight("Bob", "BB202"));
+// console.log(system.cancelBooking("Alice", "AA101"));
