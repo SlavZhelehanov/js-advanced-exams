7efa3d91d2f983cb44c0660b4ecce59c1b7f5c02
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-20 12:53:20 +0300

    Add task 2.3: bookFlight (passengerName, flightNumber)

--- 08. JS Advanced Regular Exam - 21 October 2023/02.Flight Booking System/flightBookingSystem.js
+++ 08. JS Advanced Regular Exam - 21 October 2023/02.Flight Booking System/flightBookingSystem.js
@@ -11,6 +11,13 @@ class FlightBookingSystem {
         this.flights.push({flightNumber, destination, departureTime, price});
         return `Flight ${flightNumber} to ${destination} has been added to the system.`;
     }
+
+    bookFlight (passengerName, flightNumber) {
+        if (!this.flights.some(flight => flight.flightNumber === flightNumber)) return `Flight ${flightNumber} is not available for booking.`;
+        this.bookings.push({passengerName, flightNumber});
+        this.bookingsCount++;
+        return `Booking for passenger ${passengerName} on flight ${flightNumber} is confirmed.`;
+    }
 }
 
 // Input 1
@@ -19,3 +26,12 @@ class FlightBookingSystem {
 // console.log(system.addFlight("BB202", "New York", "10:30 AM", 180));
 // console.log(system.addFlight("CC303", "Chicago", "11:45 AM", 120));
 // console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
+
+// Input 2
+// const system = new FlightBookingSystem("TravelWorld");
+// console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
+// console.log(system.addFlight("BB202", "New York", "10:30 AM", 180));
+// console.log(system.bookFlight("Alice", "AA101"));
+// console.log(system.bookFlight("Bob", "BB202"));
+// console.log(system.bookFlight("Charlie", "CC303"));
+
