dd6fae17d0c7027352c51c59e87841036afe21e9
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-22 12:26:33 +0300

    Add task 2.2: addItem(itemName, quantity)

--- 09. JS Advanced Retake Exam – 02 August 2023/02. Inventory Managment/InventoryManagment.js
+++ 09. JS Advanced Retake Exam – 02 August 2023/02. Inventory Managment/InventoryManagment.js
@@ -4,4 +4,19 @@ class InventoryManager {
         this.items = [];
         this.outOfStock = [];
     }
+
+    addItem(itemName, quantity) {
+        if (quantity <= 0) throw new Error("Quantity must be greater than zero.");
+        if (this.capacity === this.items.length) throw new Error("The inventory is already full.");
+        if (this.items.some((item) => item.itemName === itemName)) this.items.find(item => item.itemName === itemName).quantity += quantity;
+        else this.items.push({[itemName]: quantity});
+        return `Added ${quantity} ${itemName}(s) to the inventory.`;
+    }
 }
+
+// Input 1
+// const manager = new InventoryManager(2);
+//
+// console.log(manager.addItem("Drill", 10));
+// console.log(manager.addItem("Hammer", 5));
+// console.log(manager.addItem("Level", 3));
