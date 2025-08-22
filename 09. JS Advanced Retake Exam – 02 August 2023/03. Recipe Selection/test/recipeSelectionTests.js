24d41898bd3b2b81714541e48b8c7efa7c32ba9a
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-22 13:35:46 +0300

    Add tests for task 3.1: isTypeSuitable()

--- 09. JS Advanced Retake Exam – 02 August 2023/03. Recipe Selection/test/recipeSelectionTests.js
+++ 09. JS Advanced Retake Exam – 02 August 2023/03. Recipe Selection/test/recipeSelectionTests.js
@@ -1,6 +1,25 @@
 const {expect} = require('chai');
-const onlineStore = require('../onlineStore');
+const recipeSelection = require('../recipeSelection');
 
-describe("Tests …", function() {
+describe("Tests for recipeSelection", function () {
+    describe("isTypeSuitable()", function () {
+        it("should throw an error for invalid input types", function () {
+            expect(() => recipeSelection.isTypeSuitable(123, "product")).to.throw("Invalid input");
+            expect(() => recipeSelection.isTypeSuitable("quantity", 123)).to.throw("Invalid input");
+            expect(() => recipeSelection.isTypeSuitable(123, 321)).to.throw("Invalid input");
+        });
 
+        it("should return a message if the this recipe is not suitable for vegetarians", function () {
+            expect(recipeSelection.isTypeSuitable("Meat", "Vegetarian")).to.equal("This recipe is not suitable for vegetarians");
+        });
+
+        it("should return a message if the this recipe is not suitable for vegans", function () {
+            expect(recipeSelection.isTypeSuitable("Meat", "Vegan")).to.equal("This recipe is not suitable for vegans");
+            expect(recipeSelection.isTypeSuitable("Dairy", "Vegan")).to.equal("This recipe is not suitable for vegans");
+        });
+
+        it("should return a message if the this recipe is not suitable for your dietary restriction", function () {
+            expect(recipeSelection.isTypeSuitable("Fruit", "Everyone")).to.equal("This recipe is suitable for your dietary restriction");
+        });
+    });
 });
