df551da4c8a74f2ecff756a43a6ab06e6b33ab16
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-24 13:51:40 +0300

    Add task 1.1: Getting the information from the form

--- 10. JS Advanced Retake Exam - 05 April 2023/01.Gem Collection/resources/app.js
+++ 10. JS Advanced Retake Exam - 05 April 2023/01.Gem Collection/resources/app.js
@@ -1,5 +1,43 @@
 window.addEventListener("load", solve);
 
 function solve() {
- // TODO...
+    const gemName = document.getElementById("gem-name");
+    const color  = document.getElementById("color");
+    const carats = document.getElementById("carats");
+    const price = document.getElementById("price");
+    const type = document.getElementById("type");
+    const addBtn = document.getElementById("add-btn");
+    const previewList = document.getElementById("preview-list");
+
+    let inputs = [];
+
+    addBtn.addEventListener("click", e => {
+        e.preventDefault();
+
+        if (gemName.value.trim() !== "" &&  color.value.trim() !== '' &&  carats.value.trim() !== "" && price.value.trim() !== "" && type.value.trim()) {
+            previewList.innerHTML = `<li class="gem-info">
+<article>
+<h4>${gemName.value.trim()}</h4>
+<p>Color: ${color.value.trim()}</p>
+<p>Carats: ${carats.value.trim()}</p>
+<p>Price: ${price.value.trim()}$</p>
+<p>Type: ${type.value.trim()}</p>
+</article>
+<button class="save-btn">Save to Collection</button>
+<button class="edit-btn">Edit Information</button>
+<button class="cancel-btn">Cancel</button>
+</li>`;
+            inputs.push(gemName.value.trim());
+            gemName.value = "";
+            inputs.push(color.value.trim());
+            color.value = "";
+            inputs.push(carats.value.trim());
+            carats.value = "";
+            inputs.push(price.value.trim());
+            price.value = "";
+            inputs.push(type.value.trim());
+            type.value = "";
+            addBtn.disabled = true;
+        }
+    });
 }
