2fb9257628f6d1dc197343399ebc15dd81b30f1e
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-25 15:50:58 +0300

    Add task 2.1: constructor()
    To task 2: Refurbished Smartphones

---
+++ 10. JS Advanced Retake Exam - 05 April 2023/02.Refurbished Smartphones/refurbishedSmartphones.js
@@ -0,0 +1,8 @@
+class RefurbishedSmartphones {
+    constructor(retailer) {
+        this.retailer = retailer;
+        this.availableSmartphones = [];
+        this.soldSmartphones = [];
+        this.revenue = 0;
+    }
+}
