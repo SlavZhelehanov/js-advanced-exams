d53512d28811e7037a7713bfa16debb442b797ee
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-25 16:38:03 +0300

    Add task 2.2: addSmartphone (model, storage, price, condition)

--- 10. JS Advanced Retake Exam - 05 April 2023/02.Refurbished Smartphones/refurbishedSmartphones.js
+++ 10. JS Advanced Retake Exam - 05 April 2023/02.Refurbished Smartphones/refurbishedSmartphones.js
@@ -5,4 +5,20 @@ class RefurbishedSmartphones {
         this.soldSmartphones = [];
         this.revenue = 0;
     }
+
+    addSmartphone(model, storage, price, condition) {
+        if (typeof model !== 'string' || 0 === model.trim().length || !Number.isInteger(storage) || storage < 0
+            || typeof price !== 'number' || price < 0 || typeof condition !== 'string' || condition.trim().length === 0) {
+            throw new Error('Invalid smartphone!');
+        }
+        this.availableSmartphones.push({model, storage, price, condition});
+        return `New smartphone added: ${model} / ${storage} GB / ${condition} condition - ${price.toFixed(2)}$`;
+    }
 }
+
+// Input 1
+// let retailer = new RefurbishedSmartphones('SecondLife Devices');
+// console.log(retailer.addSmartphone('Samsung S20 Ultra', 256, 1000, 'good'));
+// console.log(retailer.addSmartphone('Iphone 12 mini', 128, 800, 'perfect'));
+// console.log(retailer.addSmartphone('', 512, 1900, 'good'));
+
