0a96b6e08bdef7fe4ae250626aeecf64095f9818
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-25 17:05:57 +0300

    Add task 2.3: sellSmartphone (model, desiredStorage)

--- 10. JS Advanced Retake Exam - 05 April 2023/02.Refurbished Smartphones/refurbishedSmartphones.js
+++ 10. JS Advanced Retake Exam - 05 April 2023/02.Refurbished Smartphones/refurbishedSmartphones.js
@@ -14,6 +14,17 @@ class RefurbishedSmartphones {
         this.availableSmartphones.push({model, storage, price, condition});
         return `New smartphone added: ${model} / ${storage} GB / ${condition} condition - ${price.toFixed(2)}$`;
     }
+
+    sellSmartphone(model, desiredStorage) {
+        if (!this.availableSmartphones.some(phone => phone.model === model)) throw new Error(`${model} was not found!`);
+        let phone = this.availableSmartphones.find(phone => phone.model === model);
+
+        phone.price = desiredStorage <= phone.storage ? phone.price : 128 < desiredStorage - phone.storage ? phone.price * 0.8 : phone.price * 0.9;
+        this.availableSmartphones = this.availableSmartphones.filter(p => p.model !== model);
+        this.soldSmartphones.push({model, storage: phone.storage, soldPrice: phone.price});
+        this.revenue += phone.price;
+        return `${model} was sold for ${phone.price.toFixed(2)}$`;
+    }
 }
 
 // Input 1
@@ -21,4 +32,13 @@ class RefurbishedSmartphones {
 // console.log(retailer.addSmartphone('Samsung S20 Ultra', 256, 1000, 'good'));
 // console.log(retailer.addSmartphone('Iphone 12 mini', 128, 800, 'perfect'));
 // console.log(retailer.addSmartphone('', 512, 1900, 'good'));
+
+// Input 2
+// let retailer = new RefurbishedSmartphones('SecondLife Devices');
+// retailer.addSmartphone('Samsung S20 Ultra', 256, 1000, 'good');
+// retailer.addSmartphone('Iphone 12 mini', 128, 800, 'perfect');
+// retailer.addSmartphone('Xiaomi Redmi Note 10 Pro', 128, 330, 'perfect');
+// console.log(retailer.sellSmartphone('Samsung S20 Ultra', 256));
+// console.log(retailer.sellSmartphone('Xiaomi Redmi Note 10 Pro', 256));
+// console.log(retailer.sellSmartphone('Samsung Galaxy A13', 64));
 
