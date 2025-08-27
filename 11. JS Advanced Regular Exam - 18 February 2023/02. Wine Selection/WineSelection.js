9a989a0287fe66c76130e7c3d62332745bf08f2d
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-27 18:43:12 +0300

    Add task 2.3: payWineBottle( wineName, price )

--- 11. JS Advanced Regular Exam - 18 February 2023/02. Wine Selection/WineSelection.js
+++ 11. JS Advanced Regular Exam - 18 February 2023/02. Wine Selection/WineSelection.js
@@ -10,11 +10,25 @@ class WineSelection {
         this.wines.push({wineName, wineType, price, paid: false});
         return `You reserved a bottle of ${wineName} ${wineType} wine.`;
     }
+
+    payWineBottle(wineName, price) {
+        if (!this.wines.some(wine => wine.wineName === wineName)) throw new Error(`${wineName} is not in the cellar.`);
+        if (this.wines.find(wine => wine.wineName === wineName).paid) throw new Error(`${wineName} has already been paid.`);
+        this.wines.find(wine => wine.wineName === wineName).paid = true;
+        this.bill += this.wines.find(wine => wine.wineName === wineName).price;
+        return `You bought a ${wineName} for a {price}$.`;
+    }
 }
 
 // Input 1
-const selection = new WineSelection(2)
+// const selection = new WineSelection(2)
 // console.log(selection.reserveABottle('Sauvignon Blanc Marlborough', 'White', 50));
 // console.log(selection.reserveABottle('Cabernet Sauvignon Napa Valley', 'Red', 120));
 // console.log(selection.reserveABottle('Bodegas Godelia Mencía', 'Rose', 144));
+
+// Input 2
+// const selection = new WineSelection(2)
+// selection.reserveABottle('Sauvignon Blanc Marlborough', 'White',50);
+// console.log(selection.payWineBottle('Sauvignon Blanc Marlborough', 120));
+// console.log(selection.payWineBottle('Bodegas Godelia Mencía', 144));
 
