a5ffaea32e2c6deff45c0e22a70310d0c9d3a7a5
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-27 18:36:21 +0300

    Add task 2.2: reserveABottle (wineName, wineType, price)

--- 11. JS Advanced Regular Exam - 18 February 2023/02. Wine Selection/WineSelection.js
+++ 11. JS Advanced Regular Exam - 18 February 2023/02. Wine Selection/WineSelection.js
@@ -4,4 +4,17 @@ class WineSelection {
         this.wines = [];
         this.bill = 0;
     }
+
+    reserveABottle(wineName, wineType, price) {
+        if (this.space <= this.wines.length) throw new Error('Not enough space in the cellar.');
+        this.wines.push({wineName, wineType, price, paid: false});
+        return `You reserved a bottle of ${wineName} ${wineType} wine.`;
+    }
 }
+
+// Input 1
+const selection = new WineSelection(2)
+// console.log(selection.reserveABottle('Sauvignon Blanc Marlborough', 'White', 50));
+// console.log(selection.reserveABottle('Cabernet Sauvignon Napa Valley', 'Red', 120));
+// console.log(selection.reserveABottle('Bodegas Godelia Mencía', 'Rose', 144));
+
