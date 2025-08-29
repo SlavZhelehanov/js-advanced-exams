e675b1ac8366a4d4b5ec30edecb26827a9331413
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-29 15:40:05 +0300

    Add task 1.1: Getting the information from the form

--- 12. JS Advanced Regular Exam - 22 October 2022/01. Scary Story/resources/app.js
+++ 12. JS Advanced Regular Exam - 22 October 2022/01. Scary Story/resources/app.js
@@ -1,5 +1,32 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  //TODO ....
+    const [firstName, lastName, age,storyTitle, formBtn] = document.getElementsByTagName("input");
+    const genre = document.getElementsByTagName("select")[0];
+    const story = document.getElementsByTagName("textarea")[0];
+    const previewList = document.getElementsByTagName("ul")[0];
+
+    let inputs = [];
+
+    formBtn.addEventListener("click", e => {
+        e.preventDefault();
+
+        if (firstName.value.trim() !== "" && lastName.value.trim() !== "" && age.value.trim() !== "" && storyTitle.value.trim() !== "" && genre.value.trim() !== "" && story.value.trim() !== "") {
+            previewList.innerHTML += `<li class="story-info">
+            <article>
+            <h4>Name: ${firstName.value.trim()} ${lastName.value.trim()}</h4>
+            <p>Age: ${age.value.trim()}</p>
+            <p>Title: ${storyTitle.value.trim()}</p>
+            <p>Genre: ${genre.value.trim()}</p>
+            <p>${story.value.trim()}</p>
+</article>
+<button class="save-btn">Save Story</button>
+<button class="edit-btn">Edit Story</button>
+<button class="delete-btn">Delete Story</button>
+            </li>"`;
+            inputs.push(firstName.value.trim(), lastName.value.trim(), age.value.trim(), storyTitle.value.trim(), genre.value.trim(), story.value.trim());
+            [firstName.value, lastName.value, age.value, storyTitle.value, genre.value, story.value] = ['', '', '', '', '', ''];
+            formBtn.disabled = true;
+        }
+    });
 }
