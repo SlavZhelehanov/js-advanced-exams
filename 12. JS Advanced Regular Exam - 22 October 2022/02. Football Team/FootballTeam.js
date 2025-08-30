7284e5f13ba2bbf3e4a9f6df3f623875da5f3144
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-30 17:35:33 +0300

    Add task 2.2: newAdditions()

--- 12. JS Advanced Regular Exam - 22 October 2022/02. Football Team/FootballTeam.js
+++ 12. JS Advanced Regular Exam - 22 October 2022/02. Football Team/FootballTeam.js
@@ -1,7 +1,29 @@
-class FootballTeam {
+class footballTeam {
     constructor(clubName, country) {
         this.clubName = clubName;
         this.country = country;
         this.invitedPlayers = [];
     }
+
+    newAdditions(footballPlayers) {
+        let result = [];
+
+        footballPlayers.forEach(info => {
+            const [name, age, playerValue] = info.split('/');
+            const player = this.invitedPlayers.find(player => player.name === name);
+
+            if (!result.includes(name)) result.push(name);
+            if (!player) {
+                this.invitedPlayers.push({ name, age: +age, playerValue: +playerValue });
+            } else if (player.playerValue < playerValue) {
+                player.playerValue = playerValue;
+            }
+        });
+        return `You successfully invite ${result.join(', ')}.`;
+    }
 }
+
+// Input 1
+// let fTeam = new footballTeam("Barcelona", "Spain");
+// console.log(fTeam.newAdditions(["Kylian Mbappé/23/160", "Lionel Messi/35/50", "Pau Torres/25/52"]));
+
