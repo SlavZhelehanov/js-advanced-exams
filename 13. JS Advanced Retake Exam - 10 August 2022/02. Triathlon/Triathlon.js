19a3eea2db184134d817344b0441f3a765662afc
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-03 12:21:27 +0300

    Add task 2.2: addParticipant ()

--- 13. JS Advanced Retake Exam - 10 August 2022/02. Triathlon/Triathlon.js
+++ 13. JS Advanced Retake Exam - 10 August 2022/02. Triathlon/Triathlon.js
@@ -4,4 +4,16 @@ class Triathlon {
         this.participants = {};
         this.listOfFinalists = [];
     }
+
+    addParticipant (participantName, participantGender) {
+        if (this.participants.hasOwnProperty(participantName)) return `${participantName} has already been added to the list`;
+        this.participants[participantName] = participantGender;
+        return `A new participant has been added - ${participantName}`;
+    }
 }
+
+// Input 1
+const contest = new Triathlon("Dynamos");
+console.log(contest.addParticipant("Peter", "male"));
+console.log(contest.addParticipant("Sasha", "female"));
+console.log(contest.addParticipant("Peter", "male"));
