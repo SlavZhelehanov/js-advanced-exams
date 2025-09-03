b153be400498b8e752ca70a49f2ff92c82782c1c
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-03 12:33:30 +0300

    Add task 2.3: completeness ()

--- 13. JS Advanced Retake Exam - 10 August 2022/02. Triathlon/Triathlon.js
+++ 13. JS Advanced Retake Exam - 10 August 2022/02. Triathlon/Triathlon.js
@@ -5,15 +5,32 @@ class Triathlon {
         this.listOfFinalists = [];
     }
 
-    addParticipant (participantName, participantGender) {
+    addParticipant(participantName, participantGender) {
         if (this.participants.hasOwnProperty(participantName)) return `${participantName} has already been added to the list`;
         this.participants[participantName] = participantGender;
         return `A new participant has been added - ${participantName}`;
     }
+
+    completeness(participantName, condition) {
+        if (!this.participants.hasOwnProperty(participantName)) throw new Error(`${participantName} is not in the current participants list`);
+        if (condition < 30) throw new Error(`${participantName} is not well prepared and cannot finish any discipline`);
+        if (condition / 30 < 3) return `${participantName} could only complete ${Math.floor(condition / 30)} of the disciplines`;
+        this.listOfFinalists.push({participantName, participantGender: this.participants[participantName]});
+        return `Congratulations, ${participantName} finished the whole competition`;
+    }
 }
 
 // Input 1
+// const contest = new Triathlon("Dynamos");
+// console.log(contest.addParticipant("Peter", "male"));
+// console.log(contest.addParticipant("Sasha", "female"));
+// console.log(contest.addParticipant("Peter", "male"));
+
+// Input 2
 const contest = new Triathlon("Dynamos");
-console.log(contest.addParticipant("Peter", "male"));
-console.log(contest.addParticipant("Sasha", "female"));
-console.log(contest.addParticipant("Peter", "male"));
+// console.log(contest.addParticipant("Peter", "male"));
+// console.log(contest.addParticipant("Sasha", "female"));
+// console.log(contest.addParticipant("George", "male"));
+// console.log(contest.completeness("Peter", 100));
+// console.log(contest.completeness("Sasha", 70));
+// console.log(contest.completeness("George", 20));
