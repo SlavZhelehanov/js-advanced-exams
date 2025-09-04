585801ef563d3c1421f10a2700a6418b02cb693d
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-04 12:43:59 +0300

    Add tests for task 3.1: ageRestrictions()

--- 13. JS Advanced Retake Exam - 10 August 2022/03. Movie Theater/test/movieTheaterTest.js
+++ 13. JS Advanced Retake Exam - 10 August 2022/03. Movie Theater/test/movieTheaterTest.js
@@ -2,5 +2,22 @@ const {expect} = require('chai');
 const movieTheater = require("../movieTheater");
 
 describe("Tests for movieTheater", function () {
-
+    describe("ageRestrictions()", function () {
+        it("should admit all ages", function () {
+            expect(movieTheater.ageRestrictions("G")).to.equal("All ages admitted to watch the movie");
+        });
+        it("should send parental guidance", function () {
+            expect(movieTheater.ageRestrictions("PG")).to.equal("Parental guidance suggested! Some material may not be suitable for pre-teenagers");
+        });
+        it("should send under 17 accompanying parent need message for the movie", function () {
+            expect(movieTheater.ageRestrictions("R")).to.equal("Restricted! Under 17 requires accompanying parent or adult guardian");
+        });
+        it("should send under 17 restriction message for the movie", function () {
+            expect(movieTheater.ageRestrictions("NC-17")).to.equal("No one under 17 admitted to watch the movie");
+        });
+        it("should send no age restrictions", function () {
+            expect(movieTheater.ageRestrictions("")).to.equal("There are no age restrictions for this movie");
+            expect(movieTheater.ageRestrictions("+18")).to.equal("There are no age restrictions for this movie");
+        });
+    });
 });
