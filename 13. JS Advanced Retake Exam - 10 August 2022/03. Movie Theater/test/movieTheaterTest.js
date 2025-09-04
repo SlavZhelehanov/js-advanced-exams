3e0b3ba999f6761ca06b7187b8d783c08eae1ee4
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-04 13:20:07 +0300

    Add tests for task 3.2: moneySpent()

--- 13. JS Advanced Retake Exam - 10 August 2022/03. Movie Theater/test/movieTheaterTest.js
+++ 13. JS Advanced Retake Exam - 10 August 2022/03. Movie Theater/test/movieTheaterTest.js
@@ -20,4 +20,47 @@ describe("Tests for movieTheater", function () {
             expect(movieTheater.ageRestrictions("+18")).to.equal("There are no age restrictions for this movie");
         });
     });
+
+    describe("moneySpent()", function () {
+        it("should throw an error for invalid inputs", function () {
+            expect(() => movieTheater.moneySpent (5, 5, 5)).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (5, 5, "5")).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (5, 5, ["5"])).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (5, "5", 5)).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (5, "5", "5")).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (5, "5", ["5"])).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (5, ["5"], 5)).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (5, ["5"], "5")).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent ("5", 5, 5)).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent ("5", 5, "5")).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent ("5", 5, ["5"])).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent ("5", "5", 5)).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent ("5", "5", "5")).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent ("5", "5", ["5"])).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent ("5", ["5"], 5)).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent ("5", ["5"], "5")).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent ("5", ["5"], ["5"])).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (["5"], 5, 5)).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (["5"], 5, "5")).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (["5"], 5, ["5"])).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (["5"], "5", 5)).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (["5"], "5", "5")).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (["5"], "5", ["5"])).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (["5"], ["5"], 5)).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (["5"], ["5"], "5")).to.throw("Invalid input");
+            expect(() => movieTheater.moneySpent (["5"], ["5"], ["5"])).to.throw("Invalid input");
+        });
+        it("should apply 20% discount for total cost bigger than 50 levs", () => {
+            expect(movieTheater.moneySpent (4, [], [])).to.equal("The total cost for the purchase with applied discount is 48.00");
+            expect(movieTheater.moneySpent (1, ["Nachos", "Nachos", "Nachos", "Nachos", "Nachos", "Nachos", "Popcorn"], [])).to.equal("The total cost for the purchase with applied discount is 44.40");
+            expect(movieTheater.moneySpent (3, [], ["Soda", "Soda", "Water"])).to.equal("The total cost for the purchase with applied discount is 41.20");
+            expect(movieTheater.moneySpent (3, ["Nachos"], ["Soda"])).to.equal("The total cost for the purchase with applied discount is 42.80");
+        });
+        it("should return total cost for all purchases less or equal to 50 levs", () => {
+            expect(movieTheater.moneySpent (3, [], [])).to.equal("The total cost for the purchase is 45.00");
+            expect(movieTheater.moneySpent (2, ["Nachos", "Popcorn"], [])).to.equal("The total cost for the purchase is 40.50");
+            expect(movieTheater.moneySpent (2, [], ["Soda", "Water"])).to.equal("The total cost for the purchase is 34.00");
+            expect(movieTheater.moneySpent (2, ["Popcorn"], ["Soda"])).to.equal("The total cost for the purchase is 37.00");
+        });
+    });
 });
