c55323b10774c46af05cf2d0e7c2f7a73c95a2ec
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-05 14:34:02 +0300

    Add task 1.1: Getting the information from the form

--- 14. JS Advanced Retake Exam - 6 April 2022/01. Forum posts/Ресурси/app.js
+++ 14. JS Advanced Retake Exam - 6 April 2022/01. Forum posts/Ресурси/app.js
@@ -1,5 +1,26 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  //TODO ...
+    const postTitle = document.getElementById("post-title");
+    const postCategory = document.getElementById("post-category");
+    const postContent = document.getElementById("post-content");
+    const publishBtn = document.getElementById("publish-btn");
+    const reviewList = document.getElementById("review-list");
+
+    publishBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        if (postTitle.value !== "" && postCategory.value !== "" && postContent.value !== "") {
+            reviewList.innerHTML += `<li class="rpost">
+<article>
+<h4>${postTitle.value}</h4>
+<p>Category: ${postCategory.value}</p>
+<p>Content: ${postContent.value}</p>
+</article>
+<button class="action-btn edit">Edit</button>
+<button class="action-btn approve">Approve</button>
+</li>`;
+            [postTitle.value, postCategory.value, postContent.value] = ['', '', '', ''];
+        }
+    });
 }
