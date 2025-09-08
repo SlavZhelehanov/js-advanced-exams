6f19b759f947e0cd7db851df8cd0121c369b2bf1
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-08 18:49:30 +0300

    Add task 2.3: ripenPlant()

--- 14. JS Advanced Retake Exam - 6 April 2022/02. Garden/Garden.js
+++ 14. JS Advanced Retake Exam - 6 April 2022/02. Garden/Garden.js
@@ -11,6 +11,17 @@ class Garden {
         this.spaceAvailable -= spaceRequired;
         return `The ${plantName} has been successfully planted in the garden.`;
     }
+
+    ripenPlant(plantName, quantity) {
+        let plant = this.plants.find((p) => p.plantName === plantName);
+
+        if (!plant) throw new Error(`There is no ${plantName} in the garden.`);
+        if (plant.ripe) throw new Error(`The ${plantName} is already ripe.`);
+        if(quantity <= 0) throw new Error(`The quantity cannot be zero or negative.`);
+        plant.quantity += quantity;
+        plant.ripe = true;
+        return `${quantity} ${quantity === 1 ? plantName : plantName + 's'} has successfully ripened.`;
+    }
 }
 
 // Input 1
@@ -18,3 +29,12 @@ class Garden {
 // console.log(myGarden.addPlant('apple', 20));
 // console.log(myGarden.addPlant('orange', 200));
 // console.log(myGarden.addPlant('olive', 50));
+
+// Input 2
+// const myGarden = new Garden(250)
+// console.log(myGarden.addPlant('apple', 20));
+// console.log(myGarden.addPlant('orange', 100));
+// console.log(myGarden.addPlant('cucumber', 30));
+// console.log(myGarden.ripenPlant('apple', 10));
+// console.log(myGarden.ripenPlant('orange', 1));
+// console.log(myGarden.ripenPlant('orange', 4));
