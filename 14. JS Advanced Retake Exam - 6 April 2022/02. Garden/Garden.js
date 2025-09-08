14f7df35855e717fcc80778b117330b675fc76ca
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-08 19:09:34 +0300

    Add task 2.4: harvestPlant()

--- 14. JS Advanced Retake Exam - 6 April 2022/02. Garden/Garden.js
+++ 14. JS Advanced Retake Exam - 6 April 2022/02. Garden/Garden.js
@@ -22,6 +22,16 @@ class Garden {
         plant.ripe = true;
         return `${quantity} ${quantity === 1 ? plantName : plantName + 's'} has successfully ripened.`;
     }
+
+    harvestPlant(plantName) {
+        let plant = this.plants.find((p) => p.plantName === plantName);
+
+        if (!plant) throw new Error(`There is no ${plantName} in the garden.`);
+        if (!plant.ripe) throw new Error(`The ${plantName} cannot be harvested before it is ripe.`);
+        this.plants = this.plants.filter(p => p.plantName !== plantName);
+        this.storage.push({plantName, quantity: plant.quantity});
+        return `The ${plantName} has been successfully harvested.`;
+    }
 }
 
 // Input 1
@@ -38,3 +48,41 @@ class Garden {
 // console.log(myGarden.ripenPlant('apple', 10));
 // console.log(myGarden.ripenPlant('orange', 1));
 // console.log(myGarden.ripenPlant('orange', 4));
+
+// Input 3
+// const myGarden = new Garden(250)
+// console.log(myGarden.addPlant('apple', 20));
+// console.log(myGarden.addPlant('orange', 100));
+// console.log(myGarden.addPlant('cucumber', 30));
+// console.log(myGarden.ripenPlant('apple', 10));
+// console.log(myGarden.ripenPlant('orange', 1));
+// console.log(myGarden.ripenPlant('olive', 30));
+
+// Input 4
+// const myGarden = new Garden(250)
+// console.log(myGarden.addPlant('apple', 20));
+// console.log(myGarden.addPlant('orange', 100));
+// console.log(myGarden.addPlant('cucumber', 30));
+// console.log(myGarden.ripenPlant('apple', 10));
+// console.log(myGarden.ripenPlant('orange', 1));
+// console.log(myGarden.ripenPlant('cucumber', -5));
+
+// Input 5
+// const myGarden = new Garden(250)
+// console.log(myGarden.addPlant('apple', 20));
+// console.log(myGarden.addPlant('orange', 200));
+// console.log(myGarden.addPlant('raspberry', 10));
+// console.log(myGarden.ripenPlant('apple', 10));
+// console.log(myGarden.ripenPlant('orange', 1));
+// console.log(myGarden.harvestPlant('apple'));
+// console.log(myGarden.harvestPlant('olive'));
+
+// Input 6
+// const myGarden = new Garden(250)
+// console.log(myGarden.addPlant('apple', 20));
+// console.log(myGarden.addPlant('orange', 200));
+// console.log(myGarden.addPlant('raspberry', 10));
+// console.log(myGarden.ripenPlant('apple', 10));
+// console.log(myGarden.ripenPlant('orange', 1));
+// console.log(myGarden.harvestPlant('apple'));
+// console.log(myGarden.harvestPlant('raspberry'));
