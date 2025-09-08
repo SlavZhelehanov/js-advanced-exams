f0b745c86c825a5844d984ce4944b8c1414f8881
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-08 19:49:07 +0300

    Add given resources to task 3: Book selection

---
+++ 14. JS Advanced Retake Exam - 6 April 2022/03. Book selection/test/bookSelectionTest.js
@@ -0,0 +1,16 @@
+const {expect} = require('chai');
+const bookSelection = require("../bookSelection");
+
+describe("Tests for bookSelection", function () {
+    describe("isGenreSuitable()", function () {
+
+    });
+
+    describe("isItAffordable()", function () {
+
+    });
+
+    describe("suitableTitles()", function () {
+
+    });
+});
