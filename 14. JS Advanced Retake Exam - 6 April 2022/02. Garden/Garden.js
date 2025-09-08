f7adbbaf560499e543358234ab46443c8f5b1aa3
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-08 18:33:13 +0300

    Add task 2.2: addPlant()

--- 14. JS Advanced Retake Exam - 6 April 2022/02. Garden/Garden.js
+++ 14. JS Advanced Retake Exam - 6 April 2022/02. Garden/Garden.js
@@ -4,4 +4,17 @@ class Garden {
         this.plants = [];
         this.storage = [];
     }
+
+    addPlant(plantName, spaceRequired) {
+        if (this.spaceAvailable < spaceRequired) throw new Error('Not enough space in the garden.');
+        this.plants.push({plantName, spaceRequired, ripe: false, quantity: 0});
+        this.spaceAvailable -= spaceRequired;
+        return `The ${plantName} has been successfully planted in the garden.`;
+    }
 }
+
+// Input 1
+// const myGarden = new Garden(250)
+// console.log(myGarden.addPlant('apple', 20));
+// console.log(myGarden.addPlant('orange', 200));
+// console.log(myGarden.addPlant('olive', 50));
