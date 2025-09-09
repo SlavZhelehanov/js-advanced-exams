95bf81dbe5ca32c3074bd414dc2d2791a9bc141c
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-09 15:24:41 +0300

    Add task 1.1.1: Getting the information from the form

--- 15. JS Advanced Regular Exam - 13 March 2022/01. Mails Delivery/resources/app.js
+++ 15. JS Advanced Regular Exam - 13 March 2022/01. Mails Delivery/resources/app.js
@@ -1,4 +1,27 @@
 function solve() {
+    const recipientName = document.getElementById('recipientName');
+    const title = document.getElementById('title');
+    const message = document.getElementById('message');
+    const addBtn = document.getElementById('add');
+    const [listOfMails, sentMails, deletedMails] = document.getElementsByTagName("ul");
 
+    addBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (recipientName.value.trim() !== '' || title.value.trim() !== '' || message.value.trim() !== '') {
+            listOfMails.innerHTML += `<li>
+                    <h4>Title: ${title.value.trim()}</h4>
+                    <h4>Recipient Name: ${recipientName.value.trim()}</h4>
+                    <span>${message.value.trim()}</span>
+                    <div id="list-action">
+                        <button type="submit" id="send">Send</button>
+                        <button type="submit" id="delete">Delete</button>
+                    </div>
+                </li>`;
+
+            [recipientName.value, title.value, message.value] = ['', '', ''];
+        }
+    });
 }
+
 solve()
