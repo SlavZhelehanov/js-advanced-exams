3614881fae020319601ad63a2edf1ebf05b1c1c0
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-10 16:39:58 +0300

    Add task 2.2: addCar()

--- 15. JS Advanced Regular Exam - 13 March 2022/02. Car Dealership/carDealership.js
+++ 15. JS Advanced Regular Exam - 13 March 2022/02. Car Dealership/carDealership.js
@@ -5,4 +5,16 @@ class CarDealership {
         this.soldCars = [];
         this.totalIncome = 0;
     }
+
+    addCar(model, horsepower, price, mileage) {
+        if (model === '' || horsepower < 0 || !Number.isInteger(horsepower) || price < 0 || mileage < 0) throw new Error("Invalid input!");
+        this.availableCars.push({model, horsepower, price, mileage});
+        return `New car added: ${model} - ${horsepower} HP - ${mileage.toFixed(2)} km - ${price.toFixed(2)}$`;
+    }
 }
+
+// Input 1
+// let dealership = new CarDealership('SoftAuto');
+// console.log(dealership.addCar('Toyota Corolla', 100, 3500, 190000));
+// console.log(dealership.addCar('Mercedes C63', 300, 29000, 187000));
+// console.log(dealership.addCar('', 120, 4900, 240000));
