b8c3ef173f2cd256b99b5fac9ccd443c1eb12c03
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-11 16:19:29 +0300

    Add tests to task 3.2: calculatePriceOfCar()

--- 15. JS Advanced Regular Exam - 13 March 2022/03. Rent Car/test/rentCarTest.js
+++ 15. JS Advanced Regular Exam - 13 March 2022/03. Rent Car/test/rentCarTest.js
@@ -24,4 +24,26 @@ describe('rentCar Tests', function () {
                 .to.throw('There are no such models in the catalog!');
         });
     });
+
+    describe('calculatePriceOfCar', function () {
+        it('should return correct cost for valid model and days', function () {
+            expect(rentCar.calculatePriceOfCar('BMW', 2))
+                .to.equal('You choose BMW and it will cost $90!');
+        });
+
+        it('should throw error if model is not in catalog', function () {
+            expect(() => rentCar.calculatePriceOfCar('Honda', 2))
+                .to.throw('No such model in the catalog!');
+        });
+
+        it('should throw error if model is not string', function () {
+            expect(() => rentCar.calculatePriceOfCar(123, 2))
+                .to.throw('Invalid input!');
+        });
+
+        it('should throw error if days is not an integer', function () {
+            expect(() => rentCar.calculatePriceOfCar('BMW', '2'))
+                .to.throw('Invalid input!');
+        });
+    });
 });
