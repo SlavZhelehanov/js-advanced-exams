644427da871391c00804d591459264d1cf57ecd7
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-11 16:18:13 +0300

    Add tests to task 3.1: searchCar()

--- 15. JS Advanced Regular Exam - 13 March 2022/03. Rent Car/test/rentCarTest.js
+++ 15. JS Advanced Regular Exam - 13 March 2022/03. Rent Car/test/rentCarTest.js
@@ -1,2 +1,27 @@
 const {expect} = require('chai');
 const rentCar = require("../rentCar");
+
+describe('rentCar Tests', function () {
+    describe('searchCar', function () {
+        it('should return correct message if model is found', function () {
+            const shop = ["Volkswagen", "BMW", "Audi", "BMW"];
+            expect(rentCar.searchCar(shop, "BMW"))
+                .to.equal('There is 2 car of model BMW in the catalog!');
+        });
+
+        it('should throw error if shop is not an array', function () {
+            expect(() => rentCar.searchCar("not-array", "BMW"))
+                .to.throw('Invalid input!');
+        });
+
+        it('should throw error if model is not string', function () {
+            expect(() => rentCar.searchCar(["BMW"], 123))
+                .to.throw('Invalid input!');
+        });
+
+        it('should throw error if model is not found', function () {
+            expect(() => rentCar.searchCar(["BMW"], "Audi"))
+                .to.throw('There are no such models in the catalog!');
+        });
+    });
+});
