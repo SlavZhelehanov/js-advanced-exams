d11da23f8892fc55f9bd5a0297596e47285d646f
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-12 16:36:34 +0300

    Add task 1.1. Getting the information from the hired form

--- 16. JS Advanced Regular Exam - 19 February 2022/01. Work Process/resources/app.js
+++ 16. JS Advanced Regular Exam - 19 February 2022/01. Work Process/resources/app.js
@@ -1,4 +1,26 @@
 function solve() {
-    //TODO
+    const [fname, lname, email, birth, position, salary] = document.getElementsByTagName('input');
+    const addWorker = document.getElementById('add-worker');
+    const tbody = document.getElementById('tbody');
+    const sum = document.getElementById('sum');
+
+    addWorker.addEventListener('click', e => {
+        e.preventDefault();
+
+        if (fname.value !== '' && lname.value !== '' && email.value !== '' && birth.value !== '' && position.value !== '' && salary.value !== '') {
+            tbody.innerHTML += `<tr>
+<td>${fname.value}</td>
+<td>${lname.value}</td>
+<td>${email.value}</td>
+<td>${birth.value}</td>
+<td>${position.value}</td>
+<td>${salary.value}</td>
+<td><button class="fired">Fired</button> <button class="edit">Edit</button></td>
+</tr>`;
+
+            sum.textContent = (+sum.textContent + +salary.value).toFixed(2);
+            [fname.value, lname.value, email.value, birth.value, position.value, salary.value] = ['', '', '', '', '', ''];
+        }
+    });
 }
 solve()
