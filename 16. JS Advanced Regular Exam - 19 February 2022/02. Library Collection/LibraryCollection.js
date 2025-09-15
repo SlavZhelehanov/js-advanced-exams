7e988165125dd4a54c9839f59e905c5860f80266
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-15 15:34:53 +0300

    Add task 2.2: addBook()

--- 16. JS Advanced Regular Exam - 19 February 2022/02. Library Collection/LibraryCollection.js
+++ 16. JS Advanced Regular Exam - 19 February 2022/02. Library Collection/LibraryCollection.js
@@ -3,4 +3,16 @@ class LibraryCollection {
         this.capacity = capacity;
         this.books = [];
     }
+
+    addBook(bookName, bookAuthor) {
+        if (this.capacity === this.books.length) throw new Error("Not enough space in the collection.");
+        this.books.push({bookName, bookAuthor, payed: false});
+        return `The ${bookName}, with an author ${bookAuthor}, collect.`
+    }
 }
+
+// Input 1
+// const library = new LibraryCollection(2)
+// console.log(library.addBook('In Search of Lost Time', 'Marcel Proust'));
+// console.log(library.addBook('Don Quixote', 'Miguel de Cervantes'));
+// console.log(library.addBook('Ulysses', 'James Joyce'));
