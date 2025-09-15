41e0e88659732b83d3ef17536ecdcc864eefd684
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-15 17:48:57 +0300

    Add task 2.4: removeBook()

--- 16. JS Advanced Regular Exam - 19 February 2022/02. Library Collection/LibraryCollection.js
+++ 16. JS Advanced Regular Exam - 19 February 2022/02. Library Collection/LibraryCollection.js
@@ -18,6 +18,15 @@ class LibraryCollection {
         book.payed = true;
         return `${bookName} has been successfully paid.`;
     }
+
+    removeBook(bookName) {
+        let book = this.books.find((book) => book.bookName === bookName);
+
+        if (!book) throw new Error("The book, you're looking for, is not found.");
+        if (!book.payed) throw new Error(`${bookName} need to be paid before removing from the collection.`);
+        this.books = this.books.filter(book => book.bookName !== bookName);
+        return `${bookName} remove from the collection.`;
+    }
 }
 
 // Input 1
@@ -31,3 +40,11 @@ class LibraryCollection {
 // library.addBook('In Search of Lost Time', 'Marcel Proust');
 // console.log(library.payBook('In Search of Lost Time'));
 // console.log(library.payBook('Don Quixote'));
+
+// Input 3
+// const library = new LibraryCollection(2)
+// library.addBook('In Search of Lost Time', 'Marcel Proust');
+// library.addBook('Don Quixote', 'Miguel de Cervantes');
+// library.payBook('Don Quixote');
+// console.log(library.removeBook('Don Quixote'));
+// console.log(library.removeBook('In Search of Lost Time'));
