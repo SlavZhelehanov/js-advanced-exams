20d6a2d33ebbac033ee41735e76f60cc1b1ee97f
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-15 19:21:15 +0300

    Add tests to task 3.1: calcPriceOfFlowers()

--- 16. JS Advanced Regular Exam - 19 February 2022/03. Flowers Shop/test/flowerShopTests.js
+++ 16. JS Advanced Regular Exam - 19 February 2022/03. Flowers Shop/test/flowerShopTests.js
@@ -2,5 +2,24 @@ const {expect} = require('chai');
 const flowerShop = require("../flowerShop");
 
 describe('flowerShop Tests', function () {
+    describe('calcPriceOfFlowers', () => {
+        it('should throw error for invalid flower type', () => {
+            expect(() => flowerShop.calcPriceOfFlowers(123, 5, 2)).to.throw('Invalid input!');
+        });
 
+        it('should throw error for non-integer price', () => {
+            expect(() => flowerShop.calcPriceOfFlowers('Rose', '5', 2)).to.throw('Invalid input!');
+            expect(() => flowerShop.calcPriceOfFlowers('Rose', 5.5, 2)).to.throw('Invalid input!');
+        });
+
+        it('should throw error for non-integer quantity', () => {
+            expect(() => flowerShop.calcPriceOfFlowers('Rose', 5, '2')).to.throw('Invalid input!');
+            expect(() => flowerShop.calcPriceOfFlowers('Rose', 5, 2.5)).to.throw('Invalid input!');
+        });
+
+        it('should return correct total price rounded to 2 decimals', () => {
+            const result = flowerShop.calcPriceOfFlowers('Rose', 3, 4);
+            expect(result).to.equal('You need $12.00 to buy Rose!');
+        });
+    });
 });
