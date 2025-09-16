5f4312f1534e6aaabd4f3bacd56bf6dda7b8bea6
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-16 20:58:12 +0300

    Add task 1.1: Getting the information from the repair form

--- 17. JS Advanced Retake Exam - 10 December 2021/01. Service/Resources/app.js
+++ 17. JS Advanced Retake Exam - 10 December 2021/01. Service/Resources/app.js
@@ -1,5 +1,25 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    //TODO
+    const typeProduct = document.getElementById("type-product");
+    const description = document.getElementById("description");
+    const clientName = document.getElementById("client-name");
+    const clientPhone = document.getElementById("client-phone");
+    const submit = document.getElementsByTagName("form")[0].getElementsByTagName("button")[0];
+    const receivedOrders = document.getElementById("received-orders");
+
+    submit.addEventListener("click", e => {
+        e.preventDefault();
+
+        if (typeProduct.value !== "" && clientName.value !== "" && description.value !== "" && clientPhone.value !== "") {
+            receivedOrders.innerHTML += `<div class="container">
+<h2>Product type for repair: ${typeProduct.value}</h2>
+<h3>Client information: ${clientName.value}, ${clientPhone.value}</h3>
+<h4>Description of the problem: ${description.value}</h4>
+<button class="start-btn">Start repair</button>
+<button class="finish-btn" disabled>Finish repair</button>
+</div>`;
+            [typeProduct.value, description.value, clientName.value, clientPhone.value] = ["", "", "", ""];
+        }
+    });
 }
