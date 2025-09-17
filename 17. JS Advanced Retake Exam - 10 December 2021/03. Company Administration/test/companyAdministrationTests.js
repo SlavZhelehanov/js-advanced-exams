6717a86bf6e9f649cab210ba3a2112137183d34a
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-17 14:13:53 +0300

    Add tests to task 3.1: hiringEmployee()

--- 17. JS Advanced Retake Exam - 10 December 2021/03. Company Administration/test/companyAdministrationTests.js
+++ 17. JS Advanced Retake Exam - 10 December 2021/03. Company Administration/test/companyAdministrationTests.js
@@ -1,4 +1,23 @@
 const {expect} = require('chai');
 const companyAdministration = require("../companyAdministration.js");
 
-describe('companyAdministration tests', () => {});
+describe('companyAdministration tests', () => {
+    describe("hiringEmployee", function () {
+        it("should hire when position is Programmer and experience >= 3", function () {
+            expect(companyAdministration.hiringEmployee("Alice", "Programmer", 3))
+                .to.equal("Alice was successfully hired for the position Programmer.");
+            expect(companyAdministration.hiringEmployee("Bob", "Programmer", 5))
+                .to.equal("Bob was successfully hired for the position Programmer.");
+        });
+
+        it("should not approve when position is Programmer and experience < 3", function () {
+            expect(companyAdministration.hiringEmployee("Charlie", "Programmer", 1))
+                .to.equal("Charlie is not approved for this position.");
+        });
+
+        it("should throw an error for any other position", function () {
+            expect(() => companyAdministration.hiringEmployee("Daisy", "Designer", 4))
+                .to.throw("We are not looking for workers for this position.");
+        });
+    });
+});
