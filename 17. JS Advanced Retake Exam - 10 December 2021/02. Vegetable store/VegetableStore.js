464dd097ae66a89e617e51846192581dc2f7efbc
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-17 13:02:20 +0300

    Add task 2.1: loadingVegetables()

--- 17. JS Advanced Retake Exam - 10 December 2021/02. Vegetable store/VegetableStore.js
+++ 17. JS Advanced Retake Exam - 10 December 2021/02. Vegetable store/VegetableStore.js
@@ -4,4 +4,27 @@ class VegetableStore {
         this.location = location;
         this.availableProducts = [];
     }
+
+    loadingVegetables (vegetables) {
+        let output = [];
+
+        for (const data of vegetables) {
+            const [type, quantity, price] = data.split(' ');
+            const vegetable = this.availableProducts.find((product) => product.type === type);
+
+            if (!vegetable) this.availableProducts.push({type, quantity: +quantity, price: +price});
+            else {
+                vegetable.quantity += +quantity;
+
+                if (+price < vegetable.price) vegetable.price = +price;
+            }
+            if (!output.includes(type)) output.push(type);
+        }
+
+        return `Successfully added ${output.join(', ')}`;
+    }
 }
+
+// Input 1
+// let vegStore = new VegetableStore("Jerrie Munro", "1463 Pette Kyosheta, Sofia");
+// console.log(vegStore.loadingVegetables(["Okra 2.5 3.5", "Beans 10 2.8", "Celery 5.5 2.2", "Celery 0.5 2.5"]));
