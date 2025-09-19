534bf8bfb8c2d7de289d7c2fc5988c619059b06d
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-19 16:37:20 +0300

    Add task 2.3: unregisterParticipant()

--- 18. JS Advanced Regular Exam - 23 October 2021/02. Camping/Camping.js
+++ 18. JS Advanced Regular Exam - 23 October 2021/02. Camping/Camping.js
@@ -13,6 +13,12 @@ class SummerCamp {
         this.listOfParticipants.push({name, condition, power: 100, wins: 0});
         return `The ${name} was successfully registered.`;
     }
+
+    unregisterParticipant (name) {
+        if (!this.listOfParticipants.some(participant => participant.name === name)) throw new Error(`The ${name} is not registered in the camp.`);
+        this.listOfParticipants = this.listOfParticipants.filter(participant => participant.name !== name);
+        return `The ${name} removed successfully.`;
+    }
 }
 
 // Input 1
@@ -21,4 +27,10 @@ class SummerCamp {
 // console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
 // console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
 // console.log(summerCamp.registerParticipant("Leila Wolfe", "childd", 200));
+
+// Input 2
+// const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+// console.log(summerCamp.unregisterParticipant("Petar"));
+// console.log(summerCamp.unregisterParticipant("Petar Petarson"));
 
