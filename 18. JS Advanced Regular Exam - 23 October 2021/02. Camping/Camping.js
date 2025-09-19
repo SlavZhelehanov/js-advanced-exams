95d582f9736220cadb75ff8535efc70d141e26d2
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-19 16:32:25 +0300

    Add task 2.2: registerParticipant()

--- 18. JS Advanced Regular Exam - 23 October 2021/02. Camping/Camping.js
+++ 18. JS Advanced Regular Exam - 23 October 2021/02. Camping/Camping.js
@@ -1,8 +1,24 @@
-class Camping {
+class SummerCamp {
     constructor(organizer, location) {
         this.organizer = organizer;
         this.location = location;
         this.priceForTheCamp = {"child": 150, "student": 300, "collegian": 500};
         this.listOfParticipants = [];
     }
+
+    registerParticipant(name, condition, money) {
+        if (!this.priceForTheCamp[condition]) throw new Error("Unsuccessful registration at the camp.");
+        if (this.listOfParticipants.some(participant => participant.name === name)) return `The ${name} is already registered at the camp.`;
+        if (money < this.priceForTheCamp[condition]) return "The money is not enough to pay the stay at the camp.";
+        this.listOfParticipants.push({name, condition, power: 100, wins: 0});
+        return `The ${name} was successfully registered.`;
+    }
 }
+
+// Input 1
+// const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 200));
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+// console.log(summerCamp.registerParticipant("Leila Wolfe", "childd", 200));
+
