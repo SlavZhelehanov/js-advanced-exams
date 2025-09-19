4a117c233a840412e9f9c4808b99b0d83ff14f59
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-19 19:12:51 +0300

    Add task 2.4: timeToPlay()

--- 18. JS Advanced Regular Exam - 23 October 2021/02. Camping/Camping.js
+++ 18. JS Advanced Regular Exam - 23 October 2021/02. Camping/Camping.js
@@ -19,6 +19,26 @@ class SummerCamp {
         this.listOfParticipants = this.listOfParticipants.filter(participant => participant.name !== name);
         return `The ${name} removed successfully.`;
     }
+
+    timeToPlay (typeOfGame, participant1, participant2) {
+        let p1 = this.listOfParticipants.find(participant => participant.name === participant1);
+        let p2 = participant2 ? this.listOfParticipants.find(participant => participant.name === participant2) : null;
+
+        if (!p1 || (participant2 && !p2)) throw new Error("Invalid entered name/s.");
+        if (p2 && p1.condition !== p2.condition) throw new Error("Choose players with equal condition.");
+        if (typeOfGame === "Battleship" && !p2) {
+            p1.power += 20;
+            return `The ${participant1} successfully completed the game ${typeOfGame}.`;
+        } else if (typeOfGame === "WaterBalloonFights" && p2 && p2.power < p1.power) {
+            p1.wins++;
+            return `The ${participant1} is winner in the game ${typeOfGame}.`;
+        } else if (typeOfGame === "WaterBalloonFights" && p2 && p1.power < p2.power) {
+            p2.wins++;
+            return `The ${participant2} is winner in the game ${typeOfGame}.`;
+        } else if (typeOfGame === "WaterBalloonFights" && p2 && p1.power === p2.power) {
+            return "There is no winner.";
+        }
+    }
 }
 
 // Input 1
@@ -34,3 +54,11 @@ class SummerCamp {
 // console.log(summerCamp.unregisterParticipant("Petar"));
 // console.log(summerCamp.unregisterParticipant("Petar Petarson"));
 
+// Input 3
+// const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
+// console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+// console.log(summerCamp.timeToPlay("Battleship", "Petar Petarson"));
+// console.log(summerCamp.registerParticipant("Sara Dickinson", "child", 200));
+// console.log(summerCamp.timeToPlay("WaterBalloonFights", "Petar Petarson", "Sara Dickinson"));
+// console.log(summerCamp.registerParticipant("Dimitur Kostov", "student", 300));
+// console.log(summerCamp.timeToPlay("WaterBalloonFights", "Petar Petarson", "Dimitur Kostov"));
