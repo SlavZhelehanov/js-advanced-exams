1a52f245a4733366e445b055e71dcec395dd1145
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-23 15:23:16 +0300

    Add task 2.3: inviteGuest()

--- 19. JS Advanced Retake Exam - 12 August 2021/02. Art Gallery/ArtGallery.js
+++ 19. JS Advanced Retake Exam - 12 August 2021/02. Art Gallery/ArtGallery.js
@@ -15,6 +15,15 @@ class ArtGallery {
         else this.listOfArticles.push({articleModel: articleModel.toLowerCase(), articleName, quantity});
         return `Successfully added article ${articleName} with a new quantity- ${quantity}.`;
     }
+
+    inviteGuest(guestName, personality) {
+        if (this.guests.some(g => g.guestName === guestName)) throw new Error(`${guestName} has already been invited.`);
+
+        const table = {"Vip": 500, "Middle": 250};
+
+        this.guests.push({guestName, points: table.hasOwnProperty(personality) ? table[personality] : 50, purchaseArticle: 0});
+        return `You have successfully invited ${guestName}!`;
+    }
 }
 
 // Input 1
@@ -22,3 +31,9 @@ class ArtGallery {
 // console.log(artGallery.addArticle('picture', 'Mona Liza', 3));
 // console.log(artGallery.addArticle('Item', 'Ancient vase', 2));
 // console.log(artGallery.addArticle('PICTURE', 'Mona Liza', 1));
+
+// Input 2
+// const artGallery = new ArtGallery('Curtis Mayfield');
+// console.log(artGallery.inviteGuest('John', 'Vip'));
+// console.log(artGallery.inviteGuest('Peter', 'Middle'));
+// console.log(artGallery.inviteGuest('John', 'Middle'));
