83fcb93a75482eb54856753141578c665c3bceb9
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-24 16:08:46 +0300

    Add tests to task 3.1: showMovies()

--- 19. JS Advanced Retake Exam - 12 August 2021/03. Cinema/test/cinemaTests.js
+++ 19. JS Advanced Retake Exam - 12 August 2021/03. Cinema/test/cinemaTests.js
@@ -1,4 +1,18 @@
 const {expect} = require('chai');
 const cinema = require("../cinema");
 
-describe('library tests', () => {});
+describe('library tests', () => {
+    describe("showMovies()", function () {
+        it("should return message if no movies are available", function () {
+            expect(cinema.showMovies([])).to.equal("There are currently no movies to show.");
+        });
+
+        it("should return movies joined by comma and space", function () {
+            expect(cinema.showMovies(['King Kong', 'Joker'])).to.equal("King Kong, Joker");
+        });
+
+        it("should return single movie if only one exists", function () {
+            expect(cinema.showMovies(['Avatar'])).to.equal("Avatar");
+        });
+    });
+});
