fae9404befb9cde5ebdc252d20eb704c577c048f
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-25 16:35:20 +0300

    Add task 1.1: Getting the user’s personal information

--- 20. JS Advanced Regular Exam - 27 June 2021/01. Travel Agency/resources/solution.js
+++ 20. JS Advanced Regular Exam - 27 June 2021/01. Travel Agency/resources/solution.js
@@ -1,5 +1,20 @@
 window.addEventListener('load', solution);
 
 function solution() {
-  console.log('TODO: Write the missing functionality!');
+    const [fname, email, phone, address, code, submitBTN] = document.getElementById('form').getElementsByTagName('input');
+    const infoPreview = document.getElementById('infoPreview');
+    const [editBTN, continueBTN] = document.getElementsByClassName('actions')[0].getElementsByTagName('input');
+
+    submitBTN.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (fname.value !== '' && email.value !== '') { // && phone.value !== '' && address.value !== '' && code.value !== '') {
+            infoPreview.innerHTML = `<li>Full Name: ${fname.value}</li>
+<li>Email: ${email.value}</li>
+<li>Phone Number: ${phone.value}</li>
+<li>Address: ${address.value}</li>
+<li>Postal Code: ${code.value}</li>`;
+            [fname.value, email.value, phone.value, address.value, code.value, submitBTN.disabled, editBTN.disabled, continueBTN.disabled] = ['', '', '', '', '', true, false, false];
+        }
+    });
 }
