f9c416ac71365feed11bd1daa7903436dfe0e2b9
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-26 18:17:10 +0300

    Add task 2.2: loadProducts()

--- 20. JS Advanced Regular Exam - 27 June 2021/02. Restaurant/Restaurant.js
+++ 20. JS Advanced Regular Exam - 27 June 2021/02. Restaurant/Restaurant.js
@@ -5,4 +5,29 @@ class Restaurant {
         this.stockProducts = {};
         this.history = [];
     }
+
+    loadProducts(products) {
+        for (let entry of products) {
+            let [productName, productQuantity, productTotalPrice] = entry.split(' ');
+            productQuantity = Number(productQuantity);
+            productTotalPrice = Number(productTotalPrice);
+
+            if (productTotalPrice <= this.budgetMoney) {
+                if (!this.stockProducts.hasOwnProperty(productName)) {
+                    this.stockProducts[productName] = 0;
+                }
+                this.stockProducts[productName] += productQuantity;
+                this.budgetMoney -= productTotalPrice;
+
+                this.history.push(`Successfully loaded ${productQuantity} ${productName}`);
+            } else {
+                this.history.push(`There was not enough money to load ${productQuantity} ${productName}`);
+            }
+        }
+        return this.history.join('\n');
+    }
 }
+
+// Input 1
+// let kitchen = new Restaurant(1000);
+// console.log(kitchen.loadProducts(['Banana 10 5', 'Banana 20 10', 'Strawberries 50 30', 'Yogurt 10 10', 'Yogurt 500 1500', 'Honey 5 50']));
