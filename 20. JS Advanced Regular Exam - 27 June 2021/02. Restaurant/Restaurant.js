7a980662e011407ce3c4ccc5d210ac65000083ce
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-26 18:19:45 +0300

    Add task 2.3: addToMenu()

--- 20. JS Advanced Regular Exam - 27 June 2021/02. Restaurant/Restaurant.js
+++ 20. JS Advanced Regular Exam - 27 June 2021/02. Restaurant/Restaurant.js
@@ -26,8 +26,34 @@ class Restaurant {
         }
         return this.history.join('\n');
     }
+
+    addToMenu(meal, neededProducts, price) {
+        if (this.menu.hasOwnProperty(meal)) {
+            return `The ${meal} is already in the our menu, try something different.`;
+        }
+
+        this.menu[meal] = {
+            products: neededProducts.map(p => {
+                let [name, qty] = p.split(' ');
+                return { productName: name, productQuantity: Number(qty) };
+            }),
+            price: Number(price)
+        };
+
+        let mealCount = Object.keys(this.menu).length;
+
+        if (mealCount === 1) {
+            return `Great idea! Now with the ${meal} we have 1 meal in the menu, other ideas?`;
+        }
+        return `Great idea! Now with the ${meal} we have ${mealCount} meals in the menu, other ideas?`;
+    }
 }
 
 // Input 1
 // let kitchen = new Restaurant(1000);
 // console.log(kitchen.loadProducts(['Banana 10 5', 'Banana 20 10', 'Strawberries 50 30', 'Yogurt 10 10', 'Yogurt 500 1500', 'Honey 5 50']));
+
+// Input 2
+// let kitchen = new Restaurant(1000);
+// console.log(kitchen.addToMenu('frozenYogurt', ['Yogurt 1', 'Honey 1', 'Banana 1', 'Strawberries 10'], 9.99));
+// console.log(kitchen.addToMenu('Pizza', ['Flour 0.5', 'Oil 0.2', 'Yeast 0.5', 'Salt 0.1', 'Sugar 0.1', 'Tomato sauce 0.5', 'Pepperoni 1', 'Cheese 1.5'], 15.55));
