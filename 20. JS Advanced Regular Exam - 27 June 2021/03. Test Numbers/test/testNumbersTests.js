df20b8997e501a24c39b0f8ecdb0d28a5504cff4
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-28 13:40:51 +0300

    Add tests for sumNumbers()

--- 20. JS Advanced Regular Exam - 27 June 2021/03. Test Numbers/test/testNumbersTests.js
+++ 20. JS Advanced Regular Exam - 27 June 2021/03. Test Numbers/test/testNumbersTests.js
@@ -1,4 +1,23 @@
 const {expect} = require('chai');
 const testNumbers = require("../testNumbers");
 
-describe("Tests for testNumbers", () => {});
+describe("Tests for testNumbers", () => {
+    describe("sumNumbers", function() {
+        it("should return undefined if first parameter is not a number", function() {
+            expect(testNumbers.sumNumbers('a', 5)).to.be.undefined;
+        });
+
+        it("should return undefined if second parameter is not a number", function() {
+            expect(testNumbers.sumNumbers(5, 'b')).to.be.undefined;
+        });
+
+        it("should return correct sum rounded to 2 decimals for valid numbers", function() {
+            expect(testNumbers.sumNumbers(1, 2)).to.equal('3.00');
+            expect(testNumbers.sumNumbers(1.234, 2.456)).to.equal('3.69');
+        });
+
+        it("should work with negative numbers", function() {
+            expect(testNumbers.sumNumbers(-1, -2)).to.equal('-3.00');
+        });
+    });
+});
