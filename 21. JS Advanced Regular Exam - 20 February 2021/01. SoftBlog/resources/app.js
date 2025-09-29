d6f7f04dd9243814f78cb4850c6c384c93d8938a
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-09-29 19:57:49 +0300

    Add task 1.1: Create new article

--- 21. JS Advanced Regular Exam - 20 February 2021/01. SoftBlog/resources/app.js
+++ 21. JS Advanced Regular Exam - 20 February 2021/01. SoftBlog/resources/app.js
@@ -1,3 +1,23 @@
-function solve(){
-   //Todo: Write your code here!
-  }
+function solve() {
+    const creator = document.getElementById('creator');
+    const title = document.getElementById('title');
+    const category = document.getElementById('category');
+    const content = document.getElementById('content');
+    const createBtn = document.getElementsByTagName('form')[0].getElementsByTagName('button')[0];
+    const siteSectionContent = document.getElementsByClassName('site-content')[0].getElementsByTagName('section')[0];
+
+    createBtn.addEventListener('click', e => {
+        e.preventDefault();
+
+        siteSectionContent.innerHTML += `<article>
+<h1>${title.value}</h1>
+<p>Category:<strong>${category.value}</strong></p>
+<p>Creator:<strong>${creator.value}</strong></p>
+<p>${content.value}</p>
+<div class="buttons">
+<button class="btn delete">Delete</button>
+<button class="btn archive">Archive</button>
+</div>
+</article>`;
+    });
+}
