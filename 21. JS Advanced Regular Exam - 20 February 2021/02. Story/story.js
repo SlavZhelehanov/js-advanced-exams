732903230fe295b090b05d957c6f9682177f11f3
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-01 12:23:37 +0300

    Add task 2.2: getter likes()

--- 21. JS Advanced Regular Exam - 20 February 2021/02. Story/story.js
+++ 21. JS Advanced Regular Exam - 20 February 2021/02. Story/story.js
@@ -7,4 +7,10 @@ class Story {
         this.title = title;
         this.creator = creator;
     }
+
+    get likes() {
+        if (this.#likes.length === 0) return `${this.title} has 0 likes`;
+        if (this.#likes.length === 1) return `${this.#likes[0]} likes this story!`;
+        return `${this.#likes[0]} and ${this.#likes.length - 1} others like this story!`;
+    }
 }
