ddee74f3375d0495f5e8f18ae74fa07ed8020516
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-01 12:24:53 +0300

    Add task 2.3: like()

--- 21. JS Advanced Regular Exam - 20 February 2021/02. Story/story.js
+++ 21. JS Advanced Regular Exam - 20 February 2021/02. Story/story.js
@@ -13,4 +13,16 @@ class Story {
         if (this.#likes.length === 1) return `${this.#likes[0]} likes this story!`;
         return `${this.#likes[0]} and ${this.#likes.length - 1} others like this story!`;
     }
+
+    like(username) {
+        if (this.#likes.includes(username)) throw new Error("You can't like the same story twice!");
+        if (this.creator === username) throw new Error("You can't like your own story!");
+        this.#likes.push(username);
+
+        return `${username} liked ${this.title}!`;
+    }
 }
+
+let art = new Story("My Story", "Anny");
+art.like("John");
+console.log(art.likes);
