5d764d7e7a3515acef513a2d354083e6da9ce7ed
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-01 12:27:23 +0300

    Add task 2.5: comment()

--- 21. JS Advanced Regular Exam - 20 February 2021/02. Story/story.js
+++ 21. JS Advanced Regular Exam - 20 February 2021/02. Story/story.js
@@ -27,6 +27,21 @@ class Story {
         this.#likes = this.#likes.filter(user => user !== username);
         return `${username} disliked ${this.title}`;
     }
+
+    comment(username, content, id) {
+        let comment = this.#comments.find(c => c.Id === id);
+
+        if (!id || !comment) {
+            this.#comments.push({ Id: this.#ID, Username: username, Content: content, Replies: [] });
+            this.#ID++;
+            return `${username} commented on ${this.title}`;
+        }
+
+        const rID = Number(id + '.' + (comment.Replies.length + 1));
+
+        comment.Replies.push({ Id: rID, Username: username, Content: content });
+        return "You replied successfully";
+    }
 }
 
 let art = new Story("My Story", "Anny");
@@ -34,3 +49,8 @@ art.like("John");
 console.log(art.likes);
 art.dislike("John");
 console.log(art.likes);
+art.comment("Sammy", "Some Content");
+console.log(art.comment("Ammy", "New Content"));
+art.comment("Zane", "Reply", 1);
+art.comment("Jessy", "Nice :)");
+console.log(art.comment("SAmmy", "Reply@", 1));
