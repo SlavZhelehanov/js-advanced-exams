65928499b6701e519f333657671ec8a2e40e699e
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-01 12:18:25 +0300

    Add task 1.2: Archive article

--- 21. JS Advanced Regular Exam - 20 February 2021/01. SoftBlog/resources/app.js
+++ 21. JS Advanced Regular Exam - 20 February 2021/01. SoftBlog/resources/app.js
@@ -4,20 +4,35 @@ function solve() {
     const category = document.getElementById('category');
     const content = document.getElementById('content');
     const createBtn = document.getElementsByTagName('form')[0].getElementsByTagName('button')[0];
-    const siteSectionContent = document.getElementsByClassName('site-content')[0].getElementsByTagName('section')[0];
+    const posts = document.getElementsByTagName('main')[0].getElementsByTagName('section')[0];
+    const archiveSection = document.getElementsByClassName('archive-section')[0].getElementsByTagName('ol')[0]
+
+    let titles = [];
 
     createBtn.addEventListener('click', e => {
         e.preventDefault();
 
-        siteSectionContent.innerHTML += `<article>
-<h1>${title.value}</h1>
-<p>Category:<strong>${category.value}</strong></p>
-<p>Creator:<strong>${creator.value}</strong></p>
-<p>${content.value}</p>
-<div class="buttons">
-<button class="btn delete">Delete</button>
-<button class="btn archive">Archive</button>
-</div>
-</article>`;
+        posts.innerHTML += `<article>
+        <h1>${title.value}</h1>
+        <p>Category:<strong>${category.value}</strong></p>
+        <p>Creator:<strong>${creator.value}</strong></p>
+        <p>${content.value}</p>
+        <div class="buttons">
+            <button class="btn delete">Delete</button>
+            <button class="btn archive">Archive</button>
+        </div>
+        </article>`;
+    });
+
+    posts.addEventListener('click', e => {
+        if (e.target.classList.contains('archive')) {
+            const article = e.target.parentNode.parentNode;
+            const h1Title = article.getElementsByTagName('h1')[0].textContent;
+
+            titles.push(h1Title);
+            archiveSection.innerHTML = '';
+            titles.sort((a, b) => a.localeCompare(b)).forEach(t => {archiveSection.innerHTML += `<li>${t}</li>`;});
+            article.remove();
+        }
     });
 }
