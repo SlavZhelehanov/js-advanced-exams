5125b3bf2ed6f352c73a615e3bc22171e443ced1
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-02 12:41:30 +0300

    Add tests to task 3.1: powNumber()

--- 21. JS Advanced Regular Exam - 20 February 2021/03. Number Operations/test/numberOperationsTests.js
+++ 21. JS Advanced Regular Exam - 20 February 2021/03. Number Operations/test/numberOperationsTests.js
@@ -1,2 +1,19 @@
 const {expect} = require('chai');
 const numberOperations = require("../numberOperations.js");
+
+describe('Tests for numberOperations', function() {
+    describe('powNumber', function() {
+        it('should return square of a positive number', function() {
+            expect(numberOperations.powNumber(2)).to.equal(4);
+            expect(numberOperations.powNumber(5)).to.equal(25);
+        });
+
+        it('should return square of a negative number', function() {
+            expect(numberOperations.powNumber(-3)).to.equal(9);
+        });
+
+        it('should return 0 for input 0', function() {
+            expect(numberOperations.powNumber(0)).to.equal(0);
+        });
+    });
+});
