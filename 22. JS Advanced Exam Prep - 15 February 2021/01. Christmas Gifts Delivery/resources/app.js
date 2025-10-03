7d83a8629401e589a5e3c5de736a7ecc2937b5aa
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-03 17:54:01 +0300

    Add task 1.1: Add gift

--- 22. JS Advanced Exam Prep - 15 February 2021/01. Christmas Gifts Delivery/resources/app.js
+++ 22. JS Advanced Exam Prep - 15 February 2021/01. Christmas Gifts Delivery/resources/app.js
@@ -1,3 +1,22 @@
 function solution() {
-    //TO DO
+    const [addGifts, listOfGigts, sentGifts, discardedGifts] = document.getElementsByClassName('card');
+    const giftName = addGifts.getElementsByTagName('input')[0];
+    const addBtn = addGifts.getElementsByTagName('button')[0];
+    const listOfGiftsUl = listOfGigts.getElementsByTagName('ul')[0];
+
+    let allGifts = [];
+
+    addBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        allGifts.push(giftName.value);
+        listOfGiftsUl.innerHTML = '';
+        giftName.value = '';
+        allGifts.sort((a, b) => a.localeCompare(b)).forEach(gift => {
+            listOfGiftsUl.innerHTML += `<li>${gift}
+<button id="sendButton">Send</button>
+<button id="discardButton">Discard</button>
+</li>`;
+        });
+    });
 }
