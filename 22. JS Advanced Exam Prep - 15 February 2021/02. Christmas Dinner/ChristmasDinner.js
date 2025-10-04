8abaa2d6102c25c97ecf643506caa615d5907f13
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-04 17:18:17 +0300

    Add task 2.1: shopping()

--- 22. JS Advanced Exam Prep - 15 February 2021/02. Christmas Dinner/ChristmasDinner.js
+++ 22. JS Advanced Exam Prep - 15 February 2021/02. Christmas Dinner/ChristmasDinner.js
@@ -1,8 +1,27 @@
 class ChristmasDinner {
     constructor(budget) {
+        if (budget < 0) {throw new Error("The budget cannot be a negative number")}
         this.budget = budget;
         this.dishes = [];
         this.products = [];
         this.guests = {};
     }
+
+    shopping([product, price]) {
+        if (this.budget < price) throw new Error("Not enough money to buy this product");
+        this.budget -= price;
+        this.products.push(product);
+        return `You have successfully bought ${product}!`;
+    }
 }
+
+let dinner = new ChristmasDinner(300);
+
+dinner.shopping(['Salt', 1]);
+dinner.shopping(['Beans', 3]);
+dinner.shopping(['Cabbage', 4]);
+dinner.shopping(['Rice', 2]);
+dinner.shopping(['Savory', 1]);
+dinner.shopping(['Peppers', 1]);
+dinner.shopping(['Fruits', 40]);
+dinner.shopping(['Honey', 10]);
