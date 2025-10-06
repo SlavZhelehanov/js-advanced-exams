9615f5556d356b58bba1e9329a71ce7f5413c2c6
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-06 15:51:06 +0300

    Add task 1.1: Add new task

--- 23. JS Advanced Retake Exam - 08 April 2020/01. Task Manager/Skeleton/app.js
+++ 23. JS Advanced Retake Exam - 08 April 2020/01. Task Manager/Skeleton/app.js
@@ -1,3 +1,24 @@
 function solve() {
-    console.log("//TODO")
+    const task = document.getElementById("task");
+    const description = document.getElementById("description");
+    const date = document.getElementById("date");
+    const addBtn = document.getElementById("add");
+    const [_, orange, yellow, green] = document.getElementsByTagName("section");
+    const open = orange.getElementsByTagName("div")[1];
+
+    addBtn.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        if (task.value !== "" && description.value !== "" && date.value !== "") {
+            open.innerHTML += `<article>
+<h3>${task.value}</h3>
+<p>Description: ${description.value}</p>
+<p>Due Date: ${date.value}</p>
+<div class="flex">
+<button class="green">Start</button>
+<button class="red">Delete</button>
+</div>
+</article>`;
+        }
+    });
 }
