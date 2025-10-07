a97941260a94a4e1158c1e4d5e0a43817ca61acf
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-07 16:41:39 +0300

    Add tests to task 2.1: Instantiation

--- 23. JS Advanced Retake Exam - 08 April 2020/02. Repository/test/repositoryTests.js
+++ 23. JS Advanced Retake Exam - 08 April 2020/02. Repository/test/repositoryTests.js
@@ -1,10 +1,23 @@
 let { Repository } = require("../repository.js");
 
-describe("Tests …", function () {
-    describe("TODO …", function () {
-        it("TODO …", function () {
-            // TODO: …
+describe("Repository Tests", function () {
+    let properties;
+    let repository;
+
+    beforeEach(() => {
+        properties = {
+            name: "string",
+            age: "number",
+            birthday: "object"
+        };
+        repository = new Repository(properties);
+    });
+
+    describe("Instantiation", () => {
+        it("should initialize with given props and empty Map", () => {
+            expect(repository.props).to.deep.equal(properties);
+            expect(repository.data).to.be.instanceOf(Map);
+            expect(repository.count).to.equal(0);
         });
     });
-    // TODO: …
 });
