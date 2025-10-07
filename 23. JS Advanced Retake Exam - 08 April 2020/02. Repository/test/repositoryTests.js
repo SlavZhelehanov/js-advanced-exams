8f2ea2a587118bc3b89223c1e68b4f848e0861c0
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-07 16:21:33 +0300

    Add given resources to task 2. Repository

---
+++ 23. JS Advanced Retake Exam - 08 April 2020/02. Repository/test/repositoryTests.js
@@ -0,0 +1,10 @@
+let { Repository } = require("../repository.js");
+
+describe("Tests …", function () {
+    describe("TODO …", function () {
+        it("TODO …", function () {
+            // TODO: …
+        });
+    });
+    // TODO: …
+});
