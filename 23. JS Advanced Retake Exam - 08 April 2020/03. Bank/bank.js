ebbff670900ff42bdde6f2358e771251272ca030
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-08 15:51:38 +0300

    Add task 3.2: newCustomer()

--- 23. JS Advanced Retake Exam - 08 April 2020/03. Bank/bank.js
+++ 23. JS Advanced Retake Exam - 08 April 2020/03. Bank/bank.js
@@ -5,4 +5,15 @@ class Bank {
         this.#bankName = bankName;
         this.allCustomers = [];
     }
+
+    newCustomer(customer) {
+        if (this.allCustomers.some(c => c.personalId === customer.personalId)) throw new Error(`${customer.firstName} ${customer.lastName} is already our customer!`);
+        this.allCustomers.push(customer);
+        return customer;
+    }
 }
+
+let bank = new Bank("SoftUni Bank");
+
+console.log(bank.newCustomer({firstName: "Svetlin", lastName: "Nakov", personalId: 6233267}));
+console.log(bank.newCustomer({firstName: "Mihaela", lastName: "Mileva", personalId: 4151596}));
