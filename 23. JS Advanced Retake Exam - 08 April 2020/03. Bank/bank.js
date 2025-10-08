c9c68323bdc1a37619283014d83550a63f4a8584
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-08 16:06:31 +0300

    Add task 3.3: withdrawMoney()

--- 23. JS Advanced Retake Exam - 08 April 2020/03. Bank/bank.js
+++ 23. JS Advanced Retake Exam - 08 April 2020/03. Bank/bank.js
@@ -20,6 +20,15 @@ class Bank {
         else customer["totalMoney"] += amount;
         return `${customer.totalMoney}$`;
     }
+
+    withdrawMoney(personalId, amount) {
+        let customer = this.allCustomers.find(customer => customer.personalId === personalId);
+
+        if (!customer) throw new Error("We have no customer with this ID!");
+        if (customer.totalMoney < amount) throw new Error(`${customer.firstName} ${customer.lastName} does not have enough money to withdraw that amount!`);
+        customer["totalMoney"] -= amount;
+        return `${customer.totalMoney}$`;
+    }
 }
 
 let bank = new Bank("SoftUni Bank");
@@ -27,6 +36,9 @@ let bank = new Bank("SoftUni Bank");
 console.log(bank.newCustomer({firstName: "Svetlin", lastName: "Nakov", personalId: 6233267}));
 console.log(bank.newCustomer({firstName: "Mihaela", lastName: "Mileva", personalId: 4151596}));
 
-console.log(bank.depositMoney(6233267, 123456));
-console.log(bank.depositMoney(6233267, 123456));
-// console.log(bank.depositMoney(6233266, 123456)); // Throw error
+bank.depositMoney(6233267, 250);
+console.log(bank.depositMoney(6233267, 250));
+bank.depositMoney(4151596, 555);
+
+console.log(bank.withdrawMoney(6233267, 125));
+
