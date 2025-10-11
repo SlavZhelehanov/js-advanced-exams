88331802b0159d5440b286bea1ac2bb30274e27f
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-11 18:24:00 +0300

    Add tests to task 2.2: buyMovie()

--- 24. JS Advanced Retake Exam - 10 December 2019/02. Christmas Movies/test/Christmas Movie Tests.js
+++ 24. JS Advanced Retake Exam - 10 December 2019/02. Christmas Movies/test/Christmas Movie Tests.js
@@ -15,4 +15,21 @@ describe("ChristmasMovies Class Tests", function () {
             expect(christmas.actors).to.be.an('array').that.is.empty;
         });
     });
+
+    describe("buyMovie()", () => {
+        it("should add a new movie with unique actors", () => {
+            let result = christmas.buyMovie('Home Alone', ['Macaulay Culkin', 'Joe Pesci', 'Macaulay Culkin']);
+            expect(result).to.equal(
+                'You just got Home Alone to your collection in which Macaulay Culkin, Joe Pesci are taking part!'
+            );
+            expect(christmas.movieCollection).to.have.lengthOf(1);
+            expect(christmas.movieCollection[0].actors).to.deep.equal(['Macaulay Culkin', 'Joe Pesci']);
+        });
+
+        it("should throw if movie already exists", () => {
+            christmas.buyMovie('Home Alone', ['Macaulay Culkin']);
+            expect(() => christmas.buyMovie('Home Alone', ['Another Actor']))
+                .to.throw('You already own Home Alone in your collection!');
+        });
+    });
 });
