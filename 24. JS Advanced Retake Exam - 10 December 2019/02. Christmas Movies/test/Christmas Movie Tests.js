5f964ff1c83584fdd22e7add8f00db2d4cb8a7eb
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-11 18:23:28 +0300

    Add tests to task 2.1: Instantiation

--- 24. JS Advanced Retake Exam - 10 December 2019/02. Christmas Movies/test/Christmas Movie Tests.js
+++ 24. JS Advanced Retake Exam - 10 December 2019/02. Christmas Movies/test/Christmas Movie Tests.js
@@ -2,4 +2,17 @@ const { expect } = require('chai');
 const ChristmasMovies = require('../resources'); // adjust path if needed
 
 describe("ChristmasMovies Class Tests", function () {
+    let christmas;
+
+    beforeEach(() => {
+        christmas = new ChristmasMovies();
+    });
+
+    describe("Instantiation", () => {
+        it("should initialize with correct default properties", () => {
+            expect(christmas.movieCollection).to.be.an('array').that.is.empty;
+            expect(christmas.watched).to.be.an('object').that.is.empty;
+            expect(christmas.actors).to.be.an('array').that.is.empty;
+        });
+    });
 });
